da40c92100d33c40b3f97e2ae573615e407c443f
Arturo Filastò <arturo@filasto.net>
2018-01-09 17:06:16 +0100

    Remove unneeded code

--- next.config.js
+++ next.config.js
@@ -6,11 +6,6 @@ if (process.env.NODE_ENV !== 'production') {
 
 module.exports = {
   webpack: (config) => {
-    if (config.resolve.alias) {
-      // We need to remove the react-dom alias because it breaks react-tap-event-plugin in production
-      delete config.resolve.alias['react-dom']
-      delete config.resolve.alias['react']
-    }
     config.plugins.push(
       new webpack.DefinePlugin({
         'process.env.MEASUREMENTS_URL': JSON.stringify(process.env.MEASUREMENTS_URL)
