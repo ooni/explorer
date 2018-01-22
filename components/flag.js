ad68a54ba70120ee732e7a575e6e97c65639b434
Arturo Filastò <arturo@filasto.net>
2018-01-22 18:33:13 +0200

    Implement the countries view

--- components/flag.js
+++ components/flag.js
@@ -10,16 +10,27 @@ const FlagContainer = styled.div`
   overflow: hidden;
   width: 60px;
   height: 60px;
+  ${props => {
+    if (props.center === true) {
+      return 'margin: 0 auto;'
+    }
+  }}
+  ${props => {
+    if (props.border === true)  {
+      return `border: 1px solid ${props.theme.colors.black};`
+    }
+  }}
   & > svg {
     width: 60px;
     height: auto;
+    margin: 0;
   }
 `
-export const Flag = ({alpha2}) => {
+export const Flag = ({alpha2, center, border}) => {
   alpha2 = alpha2.toLowerCase()
   const FlagSvg = require('./flags/' + alpha2).default
   return (
-    <FlagContainer>
+    <FlagContainer center={center} border={border}>
       <FlagSvg viewPort="0 0 512 512" />
     </FlagContainer>
   )
--- pages/countries.js
+++ pages/countries.js
@@ -1,123 +1,106 @@
 import React from 'react'
 import Head from 'next/head'
-import Link from 'next/link'
+import NLink from 'next/link'
 import Router from 'next/router'
-import axios from 'axios'
+
+import styled from 'styled-components'
 
 import {
-  Flex, Grid, Box
-} from 'grid-styled'
-
-import {
+  Flex, Grid, Box,
+  Divider,
   Container,
-  Stat,
-  Avatar,
-  Input
-} from 'rebass'
+  Button,
+  ButtonOutline,
+  Input,
+  Select,
+  Panel,
+  PanelHeader,
+  Text,
+  InlineForm,
+  Label,
+  Link,
+  Heading
+} from 'ooni-components'
 
-
+import Flag from '../components/Flag'
 import Layout from '../components/layout'
+import NavBar from '../components/NavBar'
 
-import { colors } from '../components/layout'
+import countryUtil from 'country-util'
 
-import { sortByKey, truncateString } from '../utils'
+const CountryBlockContent = styled.div`
+  text-align: center;
+`
+
+const CountryBlock = ({countryCode}) => {
+  return (
+    <Box w={1/6} pb={3}>
+    <CountryBlockContent>
+      <Flag center border alpha2={countryCode} />
+      <Heading h={6}>{countryUtil.territoryNames[countryCode]}</Heading>
+    </CountryBlockContent>
+    </Box>
+  )
+}
+const RegionBlock = ({regionCode}) => {
+  return <div>
+  <Heading h={1}>{countryUtil.territoryNames[regionCode]}</Heading>
+
+  <Flex wrap>
+  {countryUtil.regions[regionCode].countries.map((countryCode) =>
+    <CountryBlock countryCode={countryCode} />
+  )}
+  </Flex>
+  </div>
+}
 
 export default class extends React.Component {
-  static async getInitialProps ({ req, query }) {
-    let client = axios.create({baseURL: process.env.MEASUREMENTS_URL})
-    let [countriesR] = await Promise.all([
-        client.get('/api/_/countries?by_region=1')
-    ])
-    let countries = countriesR.data.countries
-    return {countries}
-  }
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      loading: true,
-      shownCountries: props.countries
-    }
-  }
-
-  componentDidMount () {
-    this.setState({
-      loading: false,
-    })
-  }
-
   render () {
-    const { countries } = this.props
-    console.log(countries)
+    const navItems = [
+      {
+        label: 'Search',
+        href: '/search',
+        active: false
+      },
+      {
+        label: 'Results',
+        href: '/results',
+        active: false
+      },
+      {
+        label: 'Countries',
+        href: '/countries',
+        active: true
+      },
+      {
+        label: 'About',
+        href: '/about',
+        active: false
+      },
+    ]
+
     return (
       <Layout>
         <Head>
-          <title>Countries - OONI Explorer</title>
+          <title>Internet Censorship around the world - OONI Explorer</title>
         </Head>
-        <div className='mini-header'>
-          <Container>
-          <h1>Internet Censorship around the world</h1>
-          </Container>
-        </div>
-        <div className='countries-view'>
-        <Container>
-        {countries.map((countriesInRegion) => {
-          return (
-            <div className='region'>
-            <h2>{countriesInRegion[0].region}</h2>
-            <hr />
-            <Flex wrap>
-            {
-              countriesInRegion.map((country) => {
-                return (
-                  <Box w={[1/2, 1/3, 1/4, 1/6]} px={2} py={1}>
-                    <Flex align='center'>
-                      <Box mr={2}>
-                        <Avatar size={30} src={`/_/static/flags/png250px/${country.alpha_2.toLowerCase()}.png`} />
-                      </Box>
-                      <Box>
-                      <Link href={`/country?countryCode=${country.alpha_2.toUpperCase()}`} as={`/country/${country.alpha_2.toUpperCase()}`}>
-                        <a alt={country.name}>{truncateString(country.name, 20)}</a>
-                      </Link>
-                      </Box>
-                    </Flex>
-                  </Box>
-                )
-              })
-            }
-            </Flex>
-            </div>
-          )
-        })}
-        </Container>
-        </div>
-        <style jsx>{`
-          .mini-header {
-            padding-top: 30px;
-            padding-bottom: 30px;
-            font-size: 24px;
-          }
-          .region {
-            padding-bottom: 64px;
-          }
-          .region h2 {
-            padding-bottom: 12px;
-          }
-          .region hr {
-            margin-bottom: 12px;
-          }
 
-          a {
-            font-size: 12px;
-            color: #000;
-            text-decoration: none;
-            color: rgba(0, 0, 0, 0.6);
-          }
-          a:hover {
-            text-decoration: underline;
-            color: #0588CB;
-          }
-        `}</style>
+        <NavBar items={navItems} />
+
+        <Container>
+          {/* Africa */}
+          <RegionBlock regionCode='002' />
+          {/* Americas */}
+          <RegionBlock regionCode='019' />
+          {/* Asia */}
+          <RegionBlock regionCode='142' />
+          {/* Europe */}
+          <RegionBlock regionCode='150' />
+          {/* Oceania */}
+          <RegionBlock regionCode='009' />
+          {/* Antarctica */}
+          <RegionBlock regionCode='AQ' />
+        </Container>
       </Layout>
     )
   }
