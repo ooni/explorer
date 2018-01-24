92e2a60b2ffa94a60da24d10c56620c9eb14afec
Arturo Filastò <arturo@filasto.net>
2018-01-24 18:20:29 +0200

    Add placeholder about and results pages

---
+++ pages/results.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Head from 'next/head'
+import NLink from 'next/link'
+
+import styled from 'styled-components'
+
+import {
+  Flex, Grid, Box,
+  Text,
+  Heading,
+  Container
+} from 'ooni-components'
+
+import Layout from '../components/layout'
+import NavBar from '../components/NavBar'
+
+export default class extends React.Component {
+  render () {
+
+    return (
+      <Layout>
+        <Head>
+          <title>Finding - OONI Explorer</title>
+        </Head>
+
+        <NavBar />
+
+        <Container>
+        <Heading h={2}>XXX Implement Me</Heading>
+        <Text>Maybe this should be called findings?</Text>
+        </Container>
+      </Layout>
+    )
+  }
+
+}
