38182d271575c3e96670cc8ab138fe04ecd9ef49
Arturo Filastò <arturo@filasto.net>
2018-01-24 18:16:58 +0200

    Implement the country pages

--- pages/countries.js
+++ pages/countries.js
@@ -67,7 +67,7 @@ const RegionBlock = ({regionCode}) => {
 
 export default class extends React.Component {
   render () {
-  
+
     return (
       <Layout>
         <Head>
--- pages/country.js
+++ pages/country.js
@@ -6,124 +6,144 @@ import axios from 'axios'
 
 import {
   Container,
-  Stat,
   Avatar,
   Input,
+  Heading,
+  Text,
   Flex, Box, Grid
-} from 'rebass'
+} from 'ooni-components'
 
+import NavBar from '../components/NavBar'
+import Flag from '../components/flag'
 import Layout from '../components/layout'
-import { colors } from '../components/layout'
 import { toCompactNumberUnit } from '../utils'
 
 import { VictoryPie } from 'victory'
 
+import countryUtil from 'country-util'
+
+const Stat = ({label, valueWithUnit}) => {
+  return <div>
+  <Heading h={5}>{label}</Heading>
+  <Text>{valueWithUnit.value} {valueWithUnit.unit}</Text>
+  </div>
+}
+
 export default class extends React.Component {
   static async getInitialProps ({ req, query }) {
-    const cc = query.countryCode
+    const { countryCode } = query
     let client = axios.create({baseURL: process.env.MEASUREMENTS_URL})
-    let [countriesR] = await Promise.all([
-        client.get('/api/_/country/'+cc.toUpperCase())
+    let results = await Promise.all([
+      // XXX cc @darkk we should ideally have better dedicated daily dumps for this view
+      client.get('/api/_/measurement_count_by_country'),
+      client.get('/api/_/blockpages', {params: {'probe_cc': countryCode}})
     ])
-    return {country: countriesR.data}
-  }
+    console.log(results)
+    let measurementCount = results[0].data.results.filter(d => d.probe_cc === countryCode)
+    if (measurementCount.length === 0) {
+      measurementCount = 0
+    } else {
+      measurementCount = measurementCount[0].count
+    }
+    // XXX this is currently just to show something in the UI
+    let asnCount = 42
+    const unique = (value, index, self) => self.indexOf(value) === index
+    let blockedWebsites = results[1].data.results.map(d => d.input).filter(unique)
 
-  static propTypes = {
-    countryCode: React.PropTypes.string.isRequired
+    //https://api.ooni.io/api/_/measurement_count_by_country
+    //https://api.ooni.io/api/_/blockpages?probe_cc=IT
+    return {
+      measurementCount,
+      asnCount,
+      blockedWebsites,
+      countryCode,
+      countryName: countryUtil.territoryNames[countryCode]
+    }
   }
 
   render () {
-    const { country } = this.props
-
-    let measurementCount = toCompactNumberUnit(country.measurement_count)
-    let asnCount = toCompactNumberUnit(country.asn_count)
-    let blkdWebsiteCount = toCompactNumberUnit(country.blocked_website_count)
+    const {
+      measurementCount,
+      asnCount,
+      blockedWebsites,
+      countryCode,
+      countryName
+    } = this.props
 
     return (
       <Layout>
         <Head>
-          <title>Internet Censorship in {country.name} - OONI Explorer</title>
+          <title>Internet Censorship in {countryName} - OONI Explorer</title>
         </Head>
-        <div className='mini-header'>
-          <Flex align='center'>
-          <Box><Avatar src={`/_/static/flags/png250px/${country.alpha_2.toLowerCase()}.png`} /></Box>
-          <Box><h1 style={{paddingLeft: '20px'}}>{country.name}</h1></Box>
-          </Flex>
-        </div>
 
+        <NavBar />
         <Container>
+          <Flex align='center'>
+            <Box>
+              <Flag countryCode={countryCode} />
+            </Box>
+            <Box>
+            <Heading pl={2} h={1}>{countryName}</Heading>
+            </Box>
+          </Flex>
           <Flex pt={2} pb={2}>
-            <Box col={6} p={1}>
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-            </p>
+            <Box width={2/3}>
+            <Text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+            </Text>
             </Box>
-            <Box col={2} p={1}>
-            <Flex flexColumn align='left'>
-              <Box p={2}>
-                <Stat
-                  label="Measurements"
-                  value={measurementCount.value}
-                  unit={measurementCount.unit}
-                />
-              </Box>
-              <Box p={2}>
-                <Stat
-                  label="Networks"
-                  value={asnCount.value}
-                  unit={asnCount.unit}
-                />
-              </Box>
-              <Box p={2}>
-                <Stat
-                  label="Blocked websites"
-                  value={blkdWebsiteCount.value}
-                  unit={blkdWebsiteCount.unit}
-                />
-              </Box>
-            </Flex>
-            </Box>
-            <Box col={4} p={1}>
+            <Box width={1/3}>
+              <Box>
               <VictoryPie
                 data={[
-                  {month: "Sep", profit: 35000, loss: 2000},
-                  {month: "Oct", profit: 42000, loss: 8000},
-                  {month: "Nov", profit: 55000, loss: 5000}
+                  {category: "MMED", count: 40},
+                  {category: "NEWS", count: 20},
+                  {category: "ALDR", count: 20}
                 ]}
-                x="month"
-                y={(datum) => datum.profit - datum.loss}
+                x="category"
+                y="count"
               />
+              </Box>
+              <Flex flexColumn align='left'>
+                <Box p={2}>
+                  <Stat
+                    label="Measurements"
+                    valueWithUnit={toCompactNumberUnit(measurementCount)}
+                  />
+                </Box>
+                <Box p={2}>
+                  <Stat
+                    label="Networks"
+                    valueWithUnit={toCompactNumberUnit(asnCount)}
+                  />
+                </Box>
+                <Box p={2}>
+                  <Stat
+                    label="Blocked websites"
+                    valueWithUnit={toCompactNumberUnit(blockedWebsites.length)}
+                  />
+                </Box>
+              </Flex>
             </Box>
           </Flex>
           <Flex pt={2} pb={2}>
-            <Box col={6} p={1}>
-              <h2>Blocked sites</h2>
-              <ul>
-              <li>http://google.com</li>
-              <li>http://google.com</li>
-              <li>http://google.com</li>
-              <li>http://google.com</li>
-              <li>http://google.com</li>
-              <li>http://google.com</li>
-              </ul>
+            <Box w={1/2}>
+              <Heading h={3}>Blocked sites</Heading>
+              {blockedWebsites.map(url => (
+                <Text>{url}</Text>
+              ))}
             </Box>
-            <Box col={6} p={1}>
-              <h2>Censorship methods</h2>
-              <ul>
-              <li>http://google.com</li>
-              </ul>
+            <Box w={1/2}>
+              <Heading h={3}>Censorship methods</Heading>
             </Box>
           </Flex>
         </Container>
-        <style jsx>{`
-          .mini-header {
-            color: ${ colors.offWhite };
-            background-color: ${ colors.ooniBlue };
-            padding-top: 30px;
-            padding-bottom: 30px;
-            padding-left: 30px;
-          }
-        `}</style>
       </Layout>
     )
   }
