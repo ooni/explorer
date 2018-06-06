b8fe8b9063ba031a7ddacc41bc7bbbbe0eac2ea8
Arturo Filastò <arturo@filasto.net>
2018-06-06 12:55:25 +0200

    Add alias to the now deploy script

--- scripts/now-preview.sh
+++ scripts/now-preview.sh
@@ -3,4 +3,5 @@ set -ex
 
 now --token ${NOW_TOKEN} rm -y ooni-explorer || echo "nothing deleted"
 NOW_URL=$(now --token ${NOW_TOKEN} ./ -n ooni-explorer --public --npm)
+now --token ${NOW_TOKEN} alias $NOW_URL ooni-explorer-preview.now.sh
 echo "Preview available at: $NOW_URL"
