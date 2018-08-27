df41c4214d59f56f20717b0ea4b05356ec56fd4b
Sarath M S <sarath.ms@gmail.com>
2018-08-27 18:06:43 -0400

    Show placeholder text for details of tests TBD

--- components/measurement/renderDetails.js
+++ components/measurement/renderDetails.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
 import WebConnectivityDetails from './nettests/web_connectivity'
+import DefaultTestDetails from './nettests/default'
 
 const mapTestDetails = {
   web_connectivity: WebConnectivityDetails,
 }
 
 const renderDetails = (testName = 'other', testKeys) => {
-  const TestDetails = mapTestDetails[testName]
+  const TestDetails = mapTestDetails[testName] || DefaultTestDetails
   return <TestDetails testKeys={testKeys} />
 }
 
