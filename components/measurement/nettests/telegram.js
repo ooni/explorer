e13c07ae4e28216ad417c320f27ebe15fd7a3ae2
Sarath M S <sarath.ms@gmail.com>
2018-09-06 23:07:25 -0400

    Add AccessPoint status indicators to Telegram test measurement

--- components/measurement/nettests/telegram.js
+++ components/measurement/nettests/telegram.js
@@ -4,11 +4,13 @@ import {
   Container,
   Heading,
   Flex,
-  Box
+  Text,
+  Box,
+  theme
 } from 'ooni-components'
 
-const DetailsBox = ({ title, content }) => (
-  <Box w={1/2}>
+const DetailsBox = ({ title, content, ...props}) => (
+  <Box {...props}>
     <Heading h={4}>{title}</Heading>
     {content}
   </Box>
@@ -16,18 +18,56 @@ const DetailsBox = ({ title, content }) => (
 
 DetailsBox.propTypes = {
   title: PropTypes.string.isRequired,
-  content: PropTypes.element
+  content: PropTypes.element,
+}
+
+const AccessPointStatus = ({ label, ok }) => (
+  <Box>
+    <Text fontSize={0}>{label}</Text>
+    <Text
+      fontSize={3}
+      color={ok ? theme.colors.blue5 : theme.colors.red7}
+    >
+      {ok ? 'Okay' : 'Failed'}
+    </Text>
+  </Box>
+)
+
+AccessPointStatus.propTypes = {
+  label: PropTypes.string.isRequired,
+  ok: PropTypes.bool.isRequired
 }
 
 const TelegramDetails = ({ testKeys }) => {
+  const {
+    telegram_web_status,
+    telegram_tcp_blocking,
+    telegram_http_blocking
+  } = testKeys
+
+  let telegramWebOK = true
+  let telegramDesktopOK = true
+
+  if (telegram_web_status === 'blocked') {
+    telegramWebOK = false
+  }
+
+  if (telegram_tcp_blocking === true || telegram_http_blocking === true) {
+    telegramDesktopOK = false
+  }
+
   return (
     <Container>
       <Flex>
-        <DetailsBox title='Test Details' content={
-          <Box>TBA</Box>
-        } />
-        <DetailsBox title='End Point Status' content={
-          <Box>TBA</Box>
+        <DetailsBox w={1/2} title='Test Details' content={
+          <Flex>
+            <Box w={1/4}>
+              <AccessPointStatus label='Desktop App' ok={telegramDesktopOK} />
+            </Box>
+            <Box w={1/4}>
+              <AccessPointStatus label='Web App' ok={telegramWebOK} />
+            </Box>
+          </Flex>
         } />
       </Flex>
     </Container>
