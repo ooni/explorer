238cac955ad3fe899c71b452ca37fbcdbe5b3b9b
Sarath M S <sarath.ms@gmail.com>
2018-09-12 14:04:16 -0400

    Add status to Telegram measurement page

--- components/measurement/nettests/telegram.js
+++ components/measurement/nettests/telegram.js
@@ -9,6 +9,8 @@ import {
   theme
 } from 'ooni-components'
 
+import { Tick, Cross } from 'ooni-components/dist/icons'
+
 const DetailsBox = ({ title, content, ...props}) => (
   <Box {...props}>
     <Heading h={4}>{title}</Heading>
@@ -38,6 +40,26 @@ AccessPointStatus.propTypes = {
   ok: PropTypes.bool.isRequired
 }
 
+const StatusBar = ({ anomaly, hint }) => (
+  <Box mb={4} p={3} color='white' bg={anomaly ? 'yellow9' : 'green7'}>
+    <Container>
+      <Flex>
+        <Box>
+          {anomaly ? <Cross size={32} /> : <Tick size={32} />}
+        </Box>
+        <Box ml={1}>
+          <Text bold f={2}>{hint}</Text>
+        </Box>
+      </Flex>
+    </Container>
+  </Box>
+)
+
+StatusBar.propTypes = {
+  anomaly: PropTypes.string.isRequired,
+  hint: PropTypes.string.isRequired
+}
+
 const TelegramDetails = ({ testKeys }) => {
   const {
     telegram_web_status,
@@ -48,6 +70,8 @@ const TelegramDetails = ({ testKeys }) => {
 
   let telegramWebOK = true
   let telegramDesktopOK = true
+  let anomaly = false
+  let hint = 'Telegram is working'
 
   if (telegram_web_status === 'blocked') {
     telegramWebOK = false
@@ -57,35 +81,43 @@ const TelegramDetails = ({ testKeys }) => {
     telegramDesktopOK = false
   }
 
+  if (!telegramWebOK || !telegramDesktopOK) {
+    anomaly = true
+    hint = 'Telegram is not working'
+  }
+
   return (
-    <Container>
-      <Flex>
-        <DetailsBox w={1/2} title='Test Details' content={
-          <Flex>
-            <Box w={1/4}>
-              <AccessPointStatus label='Desktop App' ok={telegramDesktopOK} />
-            </Box>
-            <Box w={1/4}>
-              <AccessPointStatus label='Web App' ok={telegramWebOK} />
-            </Box>
-          </Flex>
-        } />
-        <DetailsBox w={1/2} title='Endpoint Status' content={
-          <React.Fragment>
-            {tcp_connect.length === 0 && <Text> No results</Text>}
-            {tcp_connect.map((connection, index) => (
-              <Flex key={index}>
-                <Box>
-                  <Text>Connection to <strong>{connection.ip}:{connection.port}</strong>{
-                    connection.status.success ? ' was successful' : ' failed'
-                  }</Text>
-                </Box>
-              </Flex>
-            ))}
-          </React.Fragment>
-        } />
-      </Flex>
-    </Container>
+    <React.Fragment>
+      <StatusBar anomaly={anomaly} hint={hint} />
+      <Container>
+        <Flex>
+          <DetailsBox w={1/2} title='Test Details' content={
+            <Flex>
+              <Box w={1/4}>
+                <AccessPointStatus label='Desktop App' ok={telegramDesktopOK} />
+              </Box>
+              <Box w={1/4}>
+                <AccessPointStatus label='Web App' ok={telegramWebOK} />
+              </Box>
+            </Flex>
+          } />
+          <DetailsBox w={1/2} title='Endpoint Status' content={
+            <React.Fragment>
+              {tcp_connect.length === 0 && <Text> No results</Text>}
+              {tcp_connect.map((connection, index) => (
+                <Flex key={index}>
+                  <Box>
+                    <Text>Connection to <strong>{connection.ip}:{connection.port}</strong>{
+                      connection.status.success ? ' was successful' : ' failed'
+                    }</Text>
+                  </Box>
+                </Flex>
+              ))}
+            </React.Fragment>
+          } />
+        </Flex>
+      </Container>
+    </React.Fragment>
   )
 }
 
