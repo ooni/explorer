b71c7b197707476a320c602e9709c3503438a005
Sarath M S <sarath.ms@gmail.com>
2018-12-15 00:21:26 -0500

    Update styling of common details for all measurement pages

--- components/measurement/CommonDetails.js
+++ components/measurement/CommonDetails.js
@@ -15,6 +15,7 @@ import {
 
 import MdFileDownload from 'react-icons/lib/md/file-download'
 import NoSSR from 'react-no-ssr'
+import styled from 'styled-components'
 
 // We wrap the json viewer so that we can render it only in client side rendering
 class JsonViewer extends React.Component {
@@ -33,10 +34,15 @@ JsonViewer.propTypes = {
   src: PropTypes.object.isRequired
 }
 
+const DetailBoxLabel = styled(Text)`
+  font-weight: 600;
+`
+
 const CommonDetails = ({
   measurement
 }) => {
   const {
+    report_id,
     software_name,
     software_version,
     annotations: {
@@ -45,56 +51,54 @@ const CommonDetails = ({
     }
   } = measurement
   return (
-    <Container>
+    <React.Fragment>
       <Flex my={4}>
-        <Box width={1/2} bg='WHITE'>
-          <Border color={theme.colors.gray3}>
-            <Box p={3}>
-              <Heading h={4}>Probe Metadata</Heading>
-              <Flex mb={1}>
-                <Box width={1/2}>
-                  Software
-                </Box>
-                <Box width={1/2}>
-                  <Text bold>{software_name}</Text>
-                </Box>
-              </Flex>
-              <Flex mb={1}>
-                <Box width={1/2}>
-                  Platform
-                </Box>
-                <Box width={1/2}>
-                  <Text bold>{platform ? platform : 'unknown'}</Text>
-                </Box>
-              </Flex>
-              <Flex mb={1}>
-                <Box width={1/2}>
-                  Software Version
-                </Box>
-                <Box width={1/2}>
-                  <Text bold>{software_version}</Text>
-                </Box>
-              </Flex>
-              {engine_version &&
-              <Flex mb={1}>
-                <Box width={1/2}>
-                  Measurement Kit Version
-                </Box>
-                <Box width={1/2}>
-                  <Text bold>{engine_version}</Text>
-                </Box>
-              </Flex>}
-            </Box>
-          </Border>
+        <Box width={1} bg={theme.colors.gray2}>
+          <Box p={3}>
+            <Heading h={4}>Other Details</Heading>
+            <Flex mb={1}>
+              <Box width={1/4}>
+                <DetailBoxLabel>Measurement ID</DetailBoxLabel>
+              </Box>
+              <Box>
+                <Text>{report_id}</Text>
+              </Box>
+            </Flex>
+            <Flex mb={1}>
+              <Box width={1/4}>
+                <DetailBoxLabel>Platform</DetailBoxLabel>
+              </Box>
+              <Box width={3/4}>
+                <Text bold>{platform ? platform : 'unknown'}</Text>
+              </Box>
+            </Flex>
+            <Flex mb={1}>
+              <Box width={1/4}>
+                <DetailBoxLabel>Software Version</DetailBoxLabel>
+              </Box>
+              <Box>
+                <Text bold>{software_version}</Text>
+              </Box>
+            </Flex>
+            {engine_version &&
+            <Flex mb={1}>
+              <Box width={1/4}>
+                <DetailBoxLabel>Measurement Kit Version</DetailBoxLabel>
+              </Box>
+              <Box>
+                <Text bold>{engine_version}</Text>
+              </Box>
+            </Flex>}
+          </Box>
         </Box>
       </Flex>
       <Box>
-        <Flex px={3} align='center' bg='gray1'>
+        <Flex px={3} alignItems='center' bg={theme.colors.gray2}>
           <Box>
             <Heading h={4}>Raw Measurement Data</Heading>
           </Box>
           <Box >
-            <Button fontSize={11} mx={3} px={3}><MdFileDownload />{' '}JSON</Button>
+            <Button fontSize={11} mx={3} px={3}>Download JSON</Button>
           </Box>
         </Flex>
         <Flex bg='WHITE' p={3}>
@@ -103,7 +107,7 @@ const CommonDetails = ({
           </NoSSR>
         </Flex>
       </Box>
-    </Container>
+    </React.Fragment>
   )
 }
 
