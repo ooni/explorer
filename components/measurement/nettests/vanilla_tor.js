5ac42b2ff014842c7192658cae89152d1fbe6c05
Sarath M S <sarath.ms@gmail.com>
2019-01-25 13:22:13 -0500

    Add localization support for Vanilla Tor tests

--- components/measurement/nettests/vanilla_tor.js
+++ components/measurement/nettests/vanilla_tor.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {
-  Text
+  Container,
+  Text,
+  Flex,
+  Box
 } from 'ooni-components'
+import { FormattedMessage } from 'react-intl'
+import MdCheckCircle from 'react-icons/lib/md/check-circle'
+import MdTimelapse from 'react-icons/lib/md/timelapse'
+
+import AccessPointStatus from '../AccessPointStatus'
 
 const VanillaTorDetails = ({ measurement, render }) => {
   const testKeys = measurement.test_keys
@@ -13,27 +21,42 @@ const VanillaTorDetails = ({ measurement, render }) => {
   const torProgress = testKeys.tor_progress
   const isFailure = testKeys.error !== null
   const failure = testKeys.failure
-
   return (
     render({
       status: isAnomaly ? 'anomaly' : 'reachable',
+      statusLabel: isAnomaly
+        ? <FormattedMessage id='Measurement.Status.Header.TorVanilla.Blocked' />
+        : <FormattedMessage id='Measurement.Status.Header.TorVanilla.Reachable' />,
+      statusInfo: isAnomaly
+        ? <FormattedMessage id='Measurement.Status.Hint.TorVanilla.Blocked' />
+        : <FormattedMessage id='Measurement.Status.Hint.TorVanilla.Reachable' />,
       details: (
-        <div>
-          {/*<Text>isOK: {'' + isOK}</Text>
-          <Text>isFailure: {'' + isFailure}</Text>
-          <Text>failure: {'' + failure}</Text>
-          <Text>isAnomaly: {'' + isAnomaly}</Text>
-          <Text>torVersion: {'' + torVersion}</Text>
-          <Text>torLog: {'' + torLog}</Text>
-          <Text>torProgress: {'' + torProgress}</Text>*/}
-        </div>
+        <React.Fragment>
+          <Container>
+            <Flex>
+              <AccessPointStatus
+                width={1/4}
+                icon={<MdCheckCircle />}
+                label={<FormattedMessage id='Measurement.Details.VanillaTor.Endpoint.Label.Reachability' />}
+                ok={!isAnomaly}
+              />
+              <AccessPointStatus
+                width={1/4}
+                icon={<MdTimelapse />}
+                label={<FormattedMessage id='Measurement.Details.VanillaTor.Endpoint.Label.Progress' />}
+                ok={!isAnomaly}
+                content={<FormattedMessage id='Measurement.Details.VanillaTor.Endpoint.Progress' />}
+              />
+            </Flex>
+          </Container>
+        </React.Fragment>
       )}
     )
   )
 }
 
 VanillaTorDetails.propTypes = {
-  testKeys: PropTypes.object
-}
+  measurement: PropTypes.object.isRequired,
+  render: PropTypes.func}
 
 export default VanillaTorDetails
