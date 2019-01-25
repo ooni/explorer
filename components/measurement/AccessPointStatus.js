3c8a26db43df106a9215e1fcc27fa240bad55dc6
Sarath M S <sarath.ms@gmail.com>
2019-01-25 13:21:08 -0500

    Allow custom status for Endpoint Status

--- components/measurement/AccessPointStatus.js
+++ components/measurement/AccessPointStatus.js
@@ -3,27 +3,41 @@ import PropTypes from 'prop-types'
 import { Box } from 'ooni-components'
 import { Text } from 'rebass'
 import { FormattedMessage } from 'react-intl'
+import styled from 'styled-components'
 
-const AccessPointStatus = ({ icon, label, ok }) => (
-  <Box>
-    {icon}
-    <Text fontWeight='bold' fontSize={0}>{label}</Text>
-    <Text
-      fontSize={3}
-      fontWeight={200}
-    >
-      {ok
-        ? <FormattedMessage id='Measurement.Details.Endpoint.Status.Okay' />
-        : <FormattedMessage id='Measurement.Details.Endpoint.Status.Failed' />
-      }
-    </Text>
-  </Box>
-)
+const StatusText = styled(Text)`
+  color: ${props => props.ok && props.theme.colors.yellow9}
+`
+
+const AccessPointStatus = ({ icon, label, ok , content, ...props}) => {
+  if (content === undefined) {
+    if (ok) {
+      content = <FormattedMessage id='Measurement.Details.Endpoint.Status.Okay' />
+    } else {
+      content = <FormattedMessage id='Measurement.Details.Endpoint.Status.Failed' />
+    }
+  }
+
+  return (
+    <Box {...props}>
+      {icon}
+      <Text fontWeight='bold' fontSize={0}>{label}</Text>
+      <StatusText
+        ok={!ok}
+        fontSize={3}
+        fontWeight={200}
+      >
+        {content}
+      </StatusText>
+    </Box>
+  )
+}
 
 AccessPointStatus.propTypes = {
   icon: PropTypes.element.isRequired,
   label: PropTypes.string.isRequired,
-  ok: PropTypes.bool.isRequired
+  ok: PropTypes.bool.isRequired,
+  content: PropTypes.element
 }
 
 export default AccessPointStatus
