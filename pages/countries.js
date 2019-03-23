20f905e54e105577eef9caf92335d54787409f2d
Sarath M S <sarath.ms@gmail.com>
2019-03-23 02:18:18 -0400

    Improve countries list page

--- pages/countries.js
+++ pages/countries.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import Head from 'next/head'
 import NLink from 'next/link'
-
+import axios from 'axios'
 import styled from 'styled-components'
+import { FormattedMessage, FormattedNumber } from 'react-intl'
 
 import {
   Flex, Box,
   Container,
   Link,
   Heading,
-  Text
 } from 'ooni-components'
+import { Text } from 'rebass'
 
 import Flag from '../components/flag'
 import Layout from '../components/Layout'
@@ -21,38 +22,56 @@ import countryUtil from 'country-util'
 const CountryLink = styled(Link)`
   color: ${props => props.theme.colors.black};
   text-decoration: none;
-  text-align: center;
   &:hover {
     color: ${props => props.theme.colors.blue5};
   }
 `
 
-const CountryBlock = ({countryCode}) => {
+const CountryBlock = ({countryCode, msmtCount}) => {
   const href = `/country/${countryCode}`
   return (
-    <Box width={1/6} pb={3}>
+    <Box width={[1/2, 1/4]} my={5}>
       <NLink href={href}>
         <CountryLink href={href}>
-          <Flex justifyContent='center'>
-            <Flag center border countryCode={countryCode} />
+          <Flex flexDirection='column'>
+            <Flag center border countryCode={countryCode} size={48} />
+            <Text py={2} fontSize={24} style={{height: '80px'}}>{countryUtil.territoryNames[countryCode]}</Text>
+            <Flex alignItems='center'>
+              <Text mr={2} fontSize={20} fontWeight={600} color='blue9'><FormattedNumber value={msmtCount} /></Text>
+              <Text>Measurments</Text>
+            </Flex>
           </Flex>
-          <Text pt={2}>{countryUtil.territoryNames[countryCode]}</Text>
         </CountryLink>
       </NLink>
     </Box>
   )
 }
-const RegionBlock = ({regionCode}) => {
-  const regionName = countryUtil.territoryNames[regionCode]
-  return <div>
-    <Heading h={1} id={regionName} center pb={2}>{regionName}</Heading>
 
-    <Flex flexWrap='wrap' pb={5}>
-      {countryUtil.regions[regionCode].countries.map((countryCode) =>
-        <CountryBlock key={countryCode} countryCode={countryCode} />
-      )}
-    </Flex>
-  </div>
+const RegionBlock = ({regionCode, countries}) => {
+  const regionName = countryUtil.territoryNames[regionCode]
+  // Select countries in the region where we have measuremennts from
+  const measuredCountriesInRegion = countryUtil.regions[regionCode].countries.filter((countryCode) => (
+    countries.find((item) => item.alpha_2 === countryCode)
+  ))
+
+  // When there are no measurements from the region
+  // if (measuredCountriesInRegion.length === 0) {
+  // }
+
+  return (
+    <Box my={3}>
+      <Heading h={1} id={regionName} center py={2}>{regionName}</Heading>
+
+      <Flex flexWrap='wrap' py={4}>
+        {countries
+          .filter((c => ( measuredCountriesInRegion.indexOf(c.alpha_2) > 0 )))
+          .map((country, index) => (
+            <CountryBlock key={index} countryCode={country.alpha_2} msmtCount={country.count} />
+          ))
+        }
+      </Flex>
+    </Box>
+  )
 }
 
 const JumpToContainer = styled.div`
@@ -68,7 +87,18 @@ const JumpToLink = styled.a`
 `
 
 export default class Countries extends React.Component {
+  static async getInitialProps () {
+    const client = axios.create({baseURL: process.env.MEASUREMENTS_URL}) // eslint-disable-line
+    const result = await client.get('/api/_/countries')
+    return {
+      countries: result.data.countries
+    }
+  }
+
   render () {
+    const { countries } = this.props
+    // Africa Americas Asia Europe Oceania Antarctica
+    const regions = ['002', '019', '142', '150', '009', 'AQ']
 
     return (
       <Layout>
@@ -80,7 +110,7 @@ export default class Countries extends React.Component {
 
         <JumpToContainer>
           <Container>
-            <Text color='gray6' pb={2}>Jump to continent:</Text>
+            <Text fontWeight='bold' pb={2}><FormattedMessage id='Countries.Heading.JumpToContinent' />:</Text>
             <JumpToLink href="#Africa">Africa</JumpToLink>
             <JumpToLink href="#Americas">Americas</JumpToLink>
             <JumpToLink href="#Asia">Asia</JumpToLink>
@@ -90,18 +120,11 @@ export default class Countries extends React.Component {
         </JumpToContainer>
 
         <Container>
-          {/* Africa */}
-          <RegionBlock regionCode='002' />
-          {/* Americas */}
-          <RegionBlock regionCode='019' />
-          {/* Asia */}
-          <RegionBlock regionCode='142' />
-          {/* Europe */}
-          <RegionBlock regionCode='150' />
-          {/* Oceania */}
-          <RegionBlock regionCode='009' />
-          {/* Antarctica */}
-          <RegionBlock regionCode='AQ' />
+          {
+            regions.map((regionCode, index) => (
+              <RegionBlock key={index} regionCode={regionCode} countries={countries} />
+            ))
+          }
         </Container>
       </Layout>
     )
