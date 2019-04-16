2b51bc90112c79a6ae1d99ac41d28569a5e0cb66
Sarath M S <sarath.ms@gmail.com>
2019-04-16 15:54:47 -0400

    Use git commit hash as SENTRY_RELEASE

--- next.config.js
+++ next.config.js
@@ -1,18 +1,24 @@
 /* global require, module, process */
 const withCSS = require('@zeit/next-css')
 const withSourceMaps = require('@zeit/next-source-maps')
-
+const child_process = require('child_process')
 const webpack = require('webpack')
 
 process.env.PORT = process.env.PORT || 3100
 
 module.exports = withSourceMaps(withCSS({
-  webpack: (config, {isServer}) => {
+  generateBuildId: async () => {
+    // The latest git commit hash here as buildId
+    const gitCommitHash = child_process.execSync('git rev-parse --short HEAD', { encoding: 'utf8' }).trim()
+    return gitCommitHash
+  },
+  webpack: (config, {isServer, buildId}) => {
     config.plugins.push(
       new webpack.DefinePlugin({
         'process.env.MEASUREMENTS_URL': JSON.stringify(process.env.MEASUREMENTS_URL || 'https://api.test.ooni.io'),
         'process.env.EXPLORER_URL': JSON.stringify(process.env.EXPLORER_URL  || 'http://127.0.0.1:' + process.env.PORT),
         'process.env.SENTRY_DSN': JSON.stringify(process.env.SENTRY_DSN),
+        'process.env.SENTRY_RELEASE': JSON.stringify(buildId)
       })
     )
     config.module.rules.push({
