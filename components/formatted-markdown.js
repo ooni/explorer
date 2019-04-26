4f9e9a2d22953c6db2f6195d9d4c8d6c5989ba93
Sarath M S <sarath.ms@gmail.com>
2019-04-26 13:11:37 -0400

    Add component to parse markdown in translated strings

---
+++ components/formatted-markdown.js
@@ -0,0 +1,19 @@
+//
+import React from 'react'
+import PropTypes from 'prop-types'
+import { injectIntl, intlShape } from 'react-intl'
+import Markdown from 'markdown-to-jsx'
+
+const FormattedMarkdown = ({ intl, id }) => (
+  // <Markdown source={intl.formatMessage({ id })} />
+  <Markdown>
+    {intl.formatMessage({ id })}
+  </Markdown>
+)
+
+FormattedMarkdown.propTypes = {
+  intl: intlShape,
+  id: PropTypes.string.isRequired
+}
+
+export default injectIntl(FormattedMarkdown)
