eca02164a62bb1b2ca089cbd4ea8b1da8f7d3e05
Sarath M S <sarath.ms@gmail.com>
2019-05-01 14:33:09 -0400

    Refactor FormattedMarkdown to properly pass props to FormattedMessage

--- components/formatted-markdown.js
+++ components/formatted-markdown.js
@@ -2,20 +2,23 @@
 // https://github.com/probablyup/markdown-to-jsx
 import React from 'react'
 import PropTypes from 'prop-types'
-import { injectIntl, intlShape } from 'react-intl'
+import { FormattedMessage } from 'react-intl'
 import Markdown from 'markdown-to-jsx'
 
-const FormattedMarkdown = ({ intl, id, defaultMessage, values }) => (
-  <Markdown>
-    {intl.formatMessage({ id, defaultMessage }, values)}
-  </Markdown>
+const FormattedMarkdown = (props) => (
+  <FormattedMessage {...props} >
+    {(msg) => (
+      <Markdown>
+        {msg}
+      </Markdown>
+    )}
+  </FormattedMessage>
 )
 
 FormattedMarkdown.propTypes = {
-  intl: intlShape,
   id: PropTypes.string.isRequired,
   defaultMessage: PropTypes.string,
   values: PropTypes.object
 }
 
-export default injectIntl(FormattedMarkdown)
+export default FormattedMarkdown
