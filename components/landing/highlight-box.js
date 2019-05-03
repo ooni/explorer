580029f132d94e6a64476d96823acc53254ed987
Sarath M S <sarath.ms@gmail.com>
2019-05-03 13:44:47 -0400

    Remove empty box when title is absent in it

--- components/landing/highlight-box.js
+++ components/landing/highlight-box.js
@@ -28,9 +28,9 @@ const HighlightBox = ({
       <Text fontSize={18} fontWeight='bold' mx={1}>{countryName}</Text>
     </Flex>
     <Flex flexWrap='wrap' my={1} alignItems='center'>
-      <Box mr={2} width={[1, 'unset']} order={[1, 'unset']}>
+      {title && <Box mr={2} width={[1, 'unset']} order={[1, 'unset']}>
         <Text fontSize={18}>{title}</Text>
-      </Box>
+      </Box>}
       <Box order={[3, 'unset']} my={1}>
         <LinkButton bg='blue5' color='white' px={2} py={1}>
           <NLink href={explore}><a>Explore</a></NLink>
