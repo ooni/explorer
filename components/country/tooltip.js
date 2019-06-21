41e0f78fd51f0b38f423939a04d7fa4a9f361f34
Sarath M S <sarath.ms@gmail.com>
2019-06-21 18:30:34 -0400

    Show a vertical line when hovering over data points in charts

--- components/country/tooltip.js
+++ components/country/tooltip.js
@@ -7,17 +7,40 @@ import {
 
 import { theme } from 'ooni-components'
 
-const Tooltip = (props) => (<VictoryTooltip
-  {...props}
-  labelComponent={
-    <VictoryLabel style={{fill: theme.colors.white, fontFamily: 'Fira Sans'}}/>
-  }
-  flyoutStyle={{
-    strokeWidth: 0,
-    fill: theme.colors.gray8,
-    padding: 2,
-    pointerEvents: 'none'
-  }}
-/>)
+const Tooltip = (props) => {
+  const { x, scale } = props
+  const range = scale.y.range()
+  return (
+    <g>
+      <VictoryTooltip
+        {...props}
+        labelComponent={
+          <VictoryLabel
+            style={{
+              fill: theme.colors.white,
+              fontSize: 8
+            }}
+          />
+        }
+        flyoutStyle={{
+          strokeWidth: 0,
+          fill: theme.colors.gray8,
+          padding: 2,
+          pointerEvents: 'none'
+        }}
+      />
+      <line
+        style={{
+          stroke: 'black',
+          strokeWidth: 1
+        }}
+        x1={x}
+        x2={x}
+        y1={Math.max(...range)}
+        y2={Math.min(...range)}
+      />
+    </g>
+  )
+}
 
 export default Tooltip
