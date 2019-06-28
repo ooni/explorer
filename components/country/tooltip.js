1b060463adf6bf5982cfab536f98b45178fec9cb
Sarath M S <sarath.ms@gmail.com>
2019-06-28 14:30:24 -0400

    Use custom font in tooltip styles

--- components/country/tooltip.js
+++ components/country/tooltip.js
@@ -14,6 +14,7 @@ const Tooltip = (props) => (
       <VictoryLabel
         style={{
           fill: theme.colors.white,
+          fontFamily: '"Fira Sans", sans-serif',
           fontSize: 8
         }}
       />
