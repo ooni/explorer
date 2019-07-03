abbc040374fb63d83cdf45201be9fd27cfa9d2f4
Sarath M S <sarath.ms@gmail.com>
2019-07-03 17:54:18 -0400

    Add comment anchor target styling

--- components/country/section-header.js
+++ components/country/section-header.js
@@ -15,12 +15,15 @@ SectionHeader.Title = styled.a`
   color: ${props => props.theme.colors.blue5};
   font-size: 42px;
   font-weight: 600;
+  /* To compenstate for the sticky navigation bar
+     :target selector applies only the element with id that matches
+     the current URL fragment (e.g '/#Overview') */
   :target::before {
     content: ' ';
     display: block;
     width: 0;
+    /* NOTE: This is the combined height of the NavBar and PageNavMenu */
     height: 140px;
-    /* margin-top: -140px; */
   }
 `
 
