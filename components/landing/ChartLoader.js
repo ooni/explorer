5cfeaf25aa434efcaf67d84f1cf4d6bc3c648ea9
Arturo Filastò <arturo@filasto.net>
2019-08-01 11:37:49 -0400

    Improve ChartLoader

--- components/landing/ChartLoader.js
+++ components/landing/ChartLoader.js
@@ -10,9 +10,19 @@ export const ChartLoader = () => (
     primaryColor={theme.colors.gray3}
     secondaryColor={theme.colors.gray5}
   >
-    <rect x="10" y="0" rx="3" ry="3" width="380" height="6" />
-    <rect x="10" y="10" rx="3" ry="3" width="201" height="6" />
-    <rect x="10" y="60" rx="5" ry="5" width="5" height="125" />
-    <rect x="10" y="180" rx="5" ry="5" width="400" height="5" />
+    <rect x="20" y="17" rx="0" ry="0" width="1" height="129" />
+    <rect x="20" y="146" rx="0" ry="0" width="360" height="1" />
+    <rect x="379" y="17" rx="0" ry="0" width="1" height="129" />
+    <rect x="200" y="17" rx="0" ry="0" width="1" height="129" />
+
+    <rect x="54" y="155" rx="3" ry="3" width="16" height="4" />
+    <rect x="78" y="155" rx="3" ry="3" width="66" height="4" />
+
+    <rect x="154" y="155" rx="3" ry="3" width="16" height="4" />
+    <rect x="178" y="155" rx="3" ry="3" width="66" height="4" />
+
+    <rect x="254" y="155" rx="3" ry="3" width="16" height="4" />
+    <rect x="278" y="155" rx="3" ry="3" width="66" height="4" />
+
   </ContentLoader>
 )
