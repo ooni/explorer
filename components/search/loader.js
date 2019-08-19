9d99bcd412ebf718b639009ce8a89c80364f1db6
Arturo Filastò <arturo@filasto.net>
2019-08-19 16:24:24 +0200

    Make the loader more interesting

--- components/search/loader.js
+++ components/search/loader.js
@@ -2,45 +2,33 @@ import React from 'react'
 import ContentLoader from 'react-content-loader'
 import { theme } from 'ooni-components'
 
-export const Loader = () => (
+export const LoaderRow = (props) => {
+  const isWide = (Math.random() * 100) > 70
+  const random = Math.random() * (1 - 0.7) + 0.7
+  return (
   <ContentLoader
-    height={450}
+    height={40}
     width={450}
     speed={1}
     primaryColor={theme.colors.gray3}
     secondaryColor={theme.colors.gray5}
+    {...props}
   >
-    <circle cx="20" cy="33" r="5" />
-    <rect x="40" y="30" rx="3" ry="3" width="100" height="5" />
-    <rect x="160" y="30" rx="3" ry="3" width="70" height="5" />
-    <rect x="250" y="30" rx="3" ry="3" width="70" height="5" />
-    <circle cx="20" cy="63" r="5" />
-    <rect x="40" y="60" rx="3" ry="3" width="100" height="5" />
-    <rect x="160" y="60" rx="3" ry="3" width="70" height="5" />
-    <rect x="250" y="60" rx="3" ry="3" width="70" height="5" />
-    <circle cx="20" cy="93" r="5" />
-    <rect x="40" y="90" rx="3" ry="3" width="100" height="5" />
-    <rect x="160" y="90" rx="3" ry="3" width="70" height="5" />
-    <rect x="250" y="90" rx="3" ry="3" width="70" height="5" />
-    <circle cx="20" cy="123" r="5" />
-    <rect x="40" y="120" rx="3" ry="3" width="100" height="5" />
-    <rect x="160" y="120" rx="3" ry="3" width="70" height="5" />
-    <rect x="250" y="120" rx="3" ry="3" width="70" height="5" />
-    <circle cx="20" cy="153" r="5" />
-    <rect x="40" y="150" rx="3" ry="3" width="100" height="5" />
-    <rect x="160" y="150" rx="3" ry="3" width="70" height="5" />
-    <rect x="250" y="150" rx="3" ry="3" width="70" height="5" />
-    <circle cx="20" cy="183" r="5" />
-    <rect x="40" y="180" rx="3" ry="3" width="100" height="5" />
-    <rect x="160" y="180" rx="3" ry="3" width="70" height="5" />
-    <rect x="250" y="180" rx="3" ry="3" width="70" height="5" />
-    <circle cx="20" cy="213" r="5" />
-    <rect x="40" y="210" rx="3" ry="3" width="100" height="5" />
-    <rect x="160" y="210" rx="3" ry="3" width="70" height="5" />
-    <rect x="250" y="210" rx="3" ry="3" width="70" height="5" />
-    <circle cx="20" cy="243" r="5" />
-    <rect x="40" y="240" rx="3" ry="3" width="100" height="5" />
-    <rect x="160" y="240" rx="3" ry="3" width="70" height="5" />
-    <rect x="250" y="240" rx="3" ry="3" width="70" height="5" />
+    <rect x="0" y="15" rx="5" ry="5" width="30" height="10" />
+    <circle x="30" cx="45" cy="20" r="8" />
+    {<rect x="60" y="15" rx="5" ry="5" width={200*random} height="10" />}
+    {isWide && <rect x="280" y="15" rx="5" ry="5" width="120" height="10" />}
   </ContentLoader>
+  )
+}
+
+
+export const Loader = () => (
+  <React.Fragment>
+  {Array(10)
+  .fill('')
+  .map((e, i) => (
+    <LoaderRow key={i} style={{ opacity: Number(2 / i).toFixed(1) }} />
+  ))}
+  </React.Fragment>
 )
