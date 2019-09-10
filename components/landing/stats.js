cd126259da3cb4f429aa8cab394d829ab6f77a6b
Sarath M S <sarath.ms@gmail.com>
2019-09-10 11:36:02 -0400

    Rename 'runs' to 'measurements' in monthly stats

--- components/landing/stats.js
+++ components/landing/stats.js
@@ -35,7 +35,7 @@ class CoverageChart extends React.Component {
     this.state = {
       countryCoverage: null,
       networkCoverage: null,
-      runsByMonth: null,
+      measurementsByMonth: null,
       fetching: true
     }
   }
@@ -50,13 +50,13 @@ class CoverageChart extends React.Component {
     this.setState({
       countryCoverage: result.data.countries_by_month,
       networkCoverage: result.data.networks_by_month,
-      runsByMonth: result.data.measurements_by_month,
+      measurementsByMonth: result.data.measurements_by_month,
       fetching: false
     })
   }
 
   render() {
-    const { countryCoverage, networkCoverage, runsByMonth, fetching } = this.state
+    const { countryCoverage, networkCoverage, measurementsByMonth, fetching } = this.state
     if (fetching) {
       return (<ChartLoader />)
     }
@@ -65,14 +65,14 @@ class CoverageChart extends React.Component {
     const lastMonth = {
       countryCount: countryCoverage[countryCoverage.length - 1].value,
       networkCount: networkCoverage[networkCoverage.length - 1].value,
-      runCount: runsByMonth[runsByMonth.length - 1].value
+      measurementCount: measurementsByMonth[measurementsByMonth.length - 1].value
     }
 
     // Determine the maximum value for each data set
     // Used to scale the charts on a y-axis shared with other charts
     const countryCoverageMaxima = getMaxima(countryCoverage)
     const networkCoverageMaxima = getMaxima(networkCoverage)
-    const runsMaxima = getMaxima(runsByMonth)
+    const measurementMaxima = getMaxima(measurementsByMonth)
 
     const VictoryCursorVoronoiContainer = createContainer('cursor', 'voronoi')
 
@@ -82,7 +82,7 @@ class CoverageChart extends React.Component {
           <Text fontSize={18}>
             <FormattedMarkdown id={'Home.MonthlyStats.SummaryText'}
               values={{
-                runCount: lastMonth.runCount,
+                measurementCount: lastMonth.measurementCount,
                 networkCount: lastMonth.networkCount,
                 countryCount: lastMonth.countryCount
               }}
@@ -106,8 +106,8 @@ class CoverageChart extends React.Component {
                   return `${d.date}\n \nCountries: ${d.value}`
                 } else if (d.childName === 'networkCoverage') {
                   return `Networks: ${d.value}`
-                } else if (d.childName === 'runsByMonth') {
-                  return `Runs: ${d.value}`
+                } else if (d.childName === 'measurementsByMonth') {
+                  return `Measurements: ${d.value}`
                 }
               }}
               labelComponent={<Tooltip />}
@@ -136,7 +136,7 @@ class CoverageChart extends React.Component {
                 }
               },
               {
-                name: 'Monthly Runs',
+                name: 'Monthly Measurements',
                 symbol: {
                   type: 'minus', fill: theme.colors.yellow7
                 }
@@ -205,13 +205,13 @@ class CoverageChart extends React.Component {
               }
             }}
             tickValues={[0, 0.5, 1]}
-            tickFormat={(t) => `${Math.round(t * runsMaxima/1000, 2)}k`}
+            tickFormat={(t) => `${Math.round(t * measurementMaxima/1000, 2)}k`}
           />
           <VictoryLine
-            name='runsByMonth'
-            data={runsByMonth}
+            name='measurementsByMonth'
+            data={measurementsByMonth}
             x='date'
-            y={(d) => (d.value + 20) / runsMaxima}
+            y={(d) => (d.value + 20) / measurementMaxima}
             scale={{ x: 'time', y: 'linear' }}
             style={{
               data: {
