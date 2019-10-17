b98c7c35fa996dd4d374a9e6e0639c25425cd356
lucaspontoexe <ltx16@hotmail.com>
2019-10-17 14:59:39 -0300

    there are more problems in my git configuration

--- components/Colors.js
+++
@@ -1,7 +0,0 @@
-import { theme } from 'ooni-components'
-
-export const colorNormal = theme.colors.green7
-export const colorError = theme.colors.yellow5
-export const colorConfirmed = theme.colors.red8
-export const colorAnomaly = theme.colors.yellow8
-export const colorEmpty = theme.colors.gray3
