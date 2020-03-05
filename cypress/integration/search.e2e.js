3562610700f2a3f4dd77ff09d9b730835cc59f46
Sarath M S <sarath.ms@gmail.com>
2020-03-05 11:37:37 -0500

    Add a test for client side search

--- cypress/integration/search.e2e.js
+++ cypress/integration/search.e2e.js
@@ -8,15 +8,26 @@ describe('Seearch Page Tests', () => {
 
   // TODO: Check if all form elements are actionable
   // TODO: Check if conditional form elements render (or not) correctly
-  // TODO: Check if submitting search query returns results
-
+  
   it('default filter shows 50 results', () => {
     // Check if search results appear upon page load
-    cy.get('[data-test-id="results-list"]').children('a').should('have.length', 50)
+    cy.get('[data-test-id="results-list"]').children('a').should('have.length.above', 1)
       .each(($el) => {
         cy.wrap($el)
           .should('have.attr', 'href')
           .and('match', /measurement/)
+      })
+  })
+
+  it('shows search results when filter changes', () => {
+    cy.get('input[name="domainFilter"]').type('twitter.com')
+    cy.get('button').contains('Filter Results').click()
+    cy.get('[data-test-id="results-list"]')
+      .children('a')
+      .should('have.length.above', 1)
+      .each(($el) => {
+        cy.wrap($el)
+          .contains('twitter.com')
       })
   })
 })
