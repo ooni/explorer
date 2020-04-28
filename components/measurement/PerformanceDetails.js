bdda8c33617ff5a6dfaeb8f0efeeb958e614fa12
Krona Emmanuel <kronaemmanuel@gmail.com>
2020-04-29 01:40:37 +0500

    View for Ndt7 measurements implemented

--- components/measurement/PerformanceDetails.js
+++ components/measurement/PerformanceDetails.js
@@ -4,10 +4,13 @@ import { useIntl } from 'react-intl'
 import { DetailsBoxTable } from './DetailsBox'
 
 const PerformanceDetails = ({
+  isNdt7,
   averagePing,
   maxPing,
   mss,
   packetLoss,
+  outOfOrder,
+  timeouts
 }) => {
   const intl = useIntl()
   const items = [
@@ -17,16 +20,29 @@ const PerformanceDetails = ({
     },
     {
       label: intl.formatMessage({ id: 'Measurement.Details.Performance.Label.MaxPing' }),
-      value: '~' + maxPing.toString() + ' ms'
+      value: isNdt7 ? `~${maxPing.toString()} ms` : `${maxPing.toString()}ms`
     },
     {
       label: intl.formatMessage({ id: 'Measurement.Details.Performance.Label.MSS' }),
       value: mss.toString()
     },
     {
-      label: intl.formatMessage({ id: 'Measurement.Details.Performance.Label.PktLoss' }),
+      label: isNdt7 ? intl.formatMessage({
+          id: 'Measurement.Details.Performance.Label.RetransmitRate',
+          defaultMessage: 'RetransmitRate'
+      }): intl.formatMessage({ id: 'Measurement.Details.Performance.Label.PktLoss' }),
       value: packetLoss.toString() + '%'
     },
+    ...(isNdt7 ? [] : //only adds outOfOrder and  timeouts if isNdt7 is false
+      [{
+          label: intl.formatMessage({ id: 'Measurement.Details.Performance.Label.OutOfOrder' }),
+        value: outOfOrder.toString() + '%'
+      },
+      {
+        label: intl.formatMessage({ id: 'Measurement.Details.Performance.Label.Timeouts' }),
+        value: timeouts.toString()
+      }]
+    )
   ]
   return (
     <DetailsBoxTable
@@ -38,10 +54,13 @@ const PerformanceDetails = ({
 }
 
 PerformanceDetails.propTypes = {
+  isNdt7: PropTypes.bool.isRequired,
   averagePing: PropTypes.number.isRequired,
   maxPing: PropTypes.number.isRequired,
   mss: PropTypes.number.isRequired,
   packetLoss: PropTypes.number.isRequired,
+  outOfOrder: PropTypes.number.isRequired,
+  timeouts: PropTypes.number.isRequired
 }
 
 export default PerformanceDetails
