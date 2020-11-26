8509f618180d17708b022918c40cfe1ecbda342a
Sarath M S <sarath.ms@gmail.com>
2020-11-26 17:48:36 -0500

    Remove unnecessary custom webpack config for fonts

--- next.config.js
+++ next.config.js
@@ -13,18 +13,18 @@ module.exports = withSourceMaps({
         'process.env.SENTRY_DSN': JSON.stringify(process.env.SENTRY_DSN || 'https://49af7fff247c445b9a7c98ee21ddfd2f@sentry.io/1427510'),
       })
     )
-    config.module.rules.push({
-      test: /\.(otf|ttf|woff|woff2)$/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          limit: 10000,
-          publicPath: '/_next/static/',
-          outputPath: 'static/',
-          name: '[name].[ext]'
-        }
-      }
-    })
+    // config.module.rules.push({
+    //   test: /\.(otf|ttf|woff|woff2)$/,
+    //   use: {
+    //     loader: 'file-loader',
+    //     options: {
+    //       limit: 10000,
+    //       publicPath: '/_next/static/',
+    //       outputPath: 'static/',
+    //       name: '[name].[ext]'
+    //     }
+    //   }
+    // })
 
     if (!isServer) {
       config.resolve.alias['@sentry/node'] = '@sentry/browser'
