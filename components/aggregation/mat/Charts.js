98e707163e797f3ed51f12dbbda7d306f0935f2d
Arturo Filastò <arturo@filasto.net>
2021-03-09 17:14:52 +0100

    Show the ok_count as the difference of confirmed and anomalies

--- components/aggregation/mat/Charts.js
+++ components/aggregation/mat/Charts.js
@@ -5,7 +5,7 @@ const colorMap = {
   'confirmed_count': '#f03e3e', // red7,
   'anomaly_count': '#fab005', // yellow6
   'failure_count': '#ced4da', // gray4
-  'measurement_count': '#51cf66' // green5
+  'ok_count': '#51cf66' // green5
   // 'http-failure': '#e8590c', // orange8
 }
 
