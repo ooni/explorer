4d6a7a7d5ec7b17d348efb3286eb683fe045e426
Sarath M S <sarath.ms@gmail.com>
2021-10-15 15:40:42 -0400

    Make country page a classless component with dynamic routes

--- pages/country/[countryCode].js
+++ pages/country/[countryCode].js
@@ -1,5 +1,5 @@
 /* global process */
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import axios from 'axios'
 import {
   Container,
@@ -10,16 +10,16 @@ import countryUtil from 'country-util'
 import styled from 'styled-components'
 import { StickyContainer, Sticky } from 'react-sticky'
 
-import NavBar from '../components/NavBar'
-import Flag from '../components/Flag'
-import Layout from '../components/Layout'
-import PageNavMenu from '../components/country/PageNavMenu'
-import Overview from '../components/country/Overview'
-import WebsitesSection from '../components/country/Websites'
-import AppsSection from '../components/country/Apps'
-// import NetworkPropertiesSection from '../components/country/NetworkProperties'
-import { CountryContextProvider } from '../components/country/CountryContext'
-import CountryHead from '../components/country/CountryHead'
+import NavBar from '../../components/NavBar'
+import Flag from '../../components/Flag'
+import Layout from '../../components/Layout'
+import PageNavMenu from '../../components/country/PageNavMenu'
+import Overview from '../../components/country/Overview'
+import WebsitesSection from '../../components/country/Websites'
+import AppsSection from '../../components/country/Apps'
+// import NetworkPropertiesSection from '../../components/country/NetworkProperties'
+import { CountryContextProvider } from '../../components/country/CountryContext'
+import CountryHead from '../../components/country/CountryHead'
 
 const getCountryReports = (countryCode, data) => {
   const reports = data.filter((article) => (
@@ -42,34 +42,32 @@ const AnimatedFlex = styled(Flex)`
 const AnimatedHeading = styled(Heading)`
   transition: all 0.5s ease;
 `
-export default class Country extends React.Component {
-  static async getInitialProps ({ req, res, query }) {
-    const { countryCode } = query
+export async function getServerSideProps ({ res, query }) {
+  const { countryCode } = query
+  if (res && (countryCode !== countryCode.toUpperCase())) {
+    res.writeHead(301, {
+      Location: `/country/${countryCode.toUpperCase()}`
+    })
 
-    if (res && (countryCode !== countryCode.toUpperCase())) {
-      res.writeHead(301, {
-        Location: `/country/${countryCode.toUpperCase()}`
-      })
-
-      res.end()
-      return {}
-    }
+    res.end()
+  }
 
 
-    let client = axios.create({baseURL: process.env.NEXT_PUBLIC_MEASUREMENTS_URL}) // eslint-disable-line
-    let results = await Promise.all([
-      // XXX cc @darkk we should ideally have better dedicated daily dumps for this view
-      client.get('/api/_/test_coverage', {params: {'probe_cc': countryCode}}),
-      client.get('/api/_/country_overview', { params: {'probe_cc': countryCode}}),
-      client.get('https://ooni.org/pageindex.json')
-    ])
+  let client = axios.create({baseURL: process.env.NEXT_PUBLIC_MEASUREMENTS_URL}) // eslint-disable-line
+  let results = await Promise.all([
+    // XXX cc @darkk we should ideally have better dedicated daily dumps for this view
+    client.get('/api/_/test_coverage', {params: {'probe_cc': countryCode}}),
+    client.get('/api/_/country_overview', { params: {'probe_cc': countryCode}}),
+    client.get('https://ooni.org/pageindex.json')
+  ])
 
-    const testCoverage = results[0].data.test_coverage
-    const networkCoverage = results[0].data.network_coverage
-    const overviewStats = results[1].data
-    const reports = getCountryReports(countryCode, results[2].data)
+  const testCoverage = results[0].data.test_coverage
+  const networkCoverage = results[0].data.network_coverage
+  const overviewStats = results[1].data
+  const reports = getCountryReports(countryCode, results[2].data)
 
-    return {
+  return {
+    props: {
       testCoverage,
       networkCoverage,
       overviewStats,
@@ -78,99 +76,95 @@ export default class Country extends React.Component {
       countryName: countryUtil.territoryNames[countryCode]
     }
   }
+}
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      newData: false
-    }
-    this.fetchTestCoverageData = this.fetchTestCoverageData.bind(this)
-  }
 
-  async fetchTestCoverageData(testGroupList) {
-    let client = axios.create({baseURL: process.env.NEXT_PUBLIC_MEASUREMENTS_URL}) // eslint-disable-line
-    const result = await client.get('/api/_/test_coverage', {
-      params: {
-        'probe_cc': this.props.countryCode,
-        'test_groups': testGroupList
-      }
-    })
-    // TODO: Use React.createContext to pass along data and methods
-    this.setState({
-      newData : {
+
+const Country = ({ countryCode, countryName, overviewStats, reports, ...coverageDataSSR }) => {
+  const [newData, setNewData] = useState(false)
+
+  const fetchTestCoverageData = useCallback((testGroupList) => {
+    console.log(testGroupList)
+    const fetcher = async (testGroupList) => {
+      let client = axios.create({baseURL: process.env.NEXT_PUBLIC_MEASUREMENTS_URL}) // eslint-disable-line
+      const result = await client.get('/api/_/test_coverage', {
+        params: {
+          'probe_cc': countryCode,
+          'test_groups': testGroupList
+        }
+      })
+      // TODO: Use React.createContext to pass along data and methods
+      setNewData({
         networkCoverage: result.data.network_coverage,
         testCoverage: result.data.test_coverage
-      }
-    })
-  }
+      })
+    }
+    fetcher(testGroupList)
+    
+  }, [countryCode, setNewData])
 
-  render () {
-    const {
-      countryCode,
-      countryName,
-      overviewStats,
-      reports
-    } = this.props
+  const { testCoverage, networkCoverage } = newData !== false ? newData : coverageDataSSR
 
-    const { testCoverage, networkCoverage } = this.state.newData ? this.state.newData : this.props
+  console.log('testCoverage')
+  console.log(testCoverage[0].count)
 
-    return (
-      <Layout>
-        <CountryHead countryName={countryName} measurementCount={overviewStats.measurement_count} measuredSince={overviewStats.first_bucket_date} networkCount={overviewStats.network_count} />
-        <StickyContainer>
-          <Sticky>
-            {({ style, distanceFromTop }) => {
-              let miniHeader = false
-              if (distanceFromTop < -150) {
-                miniHeader = true
-              }
-              return (
-                <RaisedHeader style={style}>
-                  <NavBar />
-                  <Container>
-                    <AnimatedFlex alignItems='center' py={ miniHeader ? 0 : 4} flexWrap='wrap'>
-                      <Box>
-                        <Flag countryCode={countryCode} size={miniHeader ? 32: 60} />
-                      </Box>
-                      <Box ml={3} mr='auto'>
-                        <AnimatedHeading h={1} fontWeight='heading' fontSize={miniHeader ? 2 : 4}>
-                          {countryName}
-                        </AnimatedHeading>
-                      </Box>
-                      <PageNavMenu countryCode={countryCode}/>
-                    </AnimatedFlex>
-                  </Container>
-                </RaisedHeader>
-              )
-            }}
-          </Sticky>
-          <Container>
-            <Flex flexWrap='wrap' mt={4}>
-              <Box>
-                <CountryContextProvider countryCode={countryCode} countryName={countryName}>
-                  <Overview
-                    countryName={countryName}
-                    middleboxCount={overviewStats.middlebox_detected_networks}
-                    imCount={overviewStats.im_apps_blocked}
-                    circumventionTools={overviewStats.circumvention_tools_blocked}
-                    blockedWebsitesCount={overviewStats.websites_confirmed_blocked}
-                    networkCount={overviewStats.network_count}
-                    measurementCount={overviewStats.measurement_count}
-                    measuredSince={overviewStats.first_bucket_date}
-                    testCoverage={testCoverage}
-                    networkCoverage={networkCoverage}
-                    fetchTestCoverageData={this.fetchTestCoverageData}
-                    featuredArticles={reports}
-                  />
-                  <WebsitesSection />
-                  <AppsSection />
-                  {/* <NetworkPropertiesSection countryCode={countryCode} /> */}
-                </CountryContextProvider>
-              </Box>
-            </Flex>
-          </Container>
-        </StickyContainer>
-      </Layout>
-    )
-  }
+  return (
+    <Layout>
+      <CountryHead countryName={countryName} measurementCount={overviewStats.measurement_count} measuredSince={overviewStats.first_bucket_date} networkCount={overviewStats.network_count} />
+      <StickyContainer>
+        <Sticky>
+          {({ style, distanceFromTop }) => {
+            let miniHeader = false
+            if (distanceFromTop < -150) {
+              miniHeader = true
+            }
+            return (
+              <RaisedHeader style={style}>
+                <NavBar />
+                <Container>
+                  <AnimatedFlex alignItems='center' py={ miniHeader ? 0 : 4} flexWrap='wrap'>
+                    <Box>
+                      <Flag countryCode={countryCode} size={miniHeader ? 32: 60} />
+                    </Box>
+                    <Box ml={3} mr='auto'>
+                      <AnimatedHeading h={1} fontWeight='heading' fontSize={miniHeader ? 2 : 4}>
+                        {countryName}
+                      </AnimatedHeading>
+                    </Box>
+                    <PageNavMenu countryCode={countryCode}/>
+                  </AnimatedFlex>
+                </Container>
+              </RaisedHeader>
+            )
+          }}
+        </Sticky>
+        <Container>
+          <Flex flexWrap='wrap' mt={4}>
+            <Box>
+              <CountryContextProvider countryCode={countryCode} countryName={countryName}>
+                <Overview
+                  countryName={countryName}
+                  middleboxCount={overviewStats.middlebox_detected_networks}
+                  imCount={overviewStats.im_apps_blocked}
+                  circumventionTools={overviewStats.circumvention_tools_blocked}
+                  blockedWebsitesCount={overviewStats.websites_confirmed_blocked}
+                  networkCount={overviewStats.network_count}
+                  measurementCount={overviewStats.measurement_count}
+                  measuredSince={overviewStats.first_bucket_date}
+                  testCoverage={testCoverage}
+                  networkCoverage={networkCoverage}
+                  fetchTestCoverageData={fetchTestCoverageData}
+                  featuredArticles={reports}
+                />
+                <WebsitesSection />
+                <AppsSection />
+              </CountryContextProvider>
+            </Box>
+          </Flex>
+        </Container>
+      </StickyContainer>
+    </Layout>
+  )
 }
+
+export default Country
