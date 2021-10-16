459d83370daae11a393996caeb6cba89cd6e593d
Sarath M S <sarath.ms@gmail.com>
2021-10-16 02:38:47 -0400

    Remove old Sentry integration

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /* global module */
 module.exports = {
   productionBrowserSourceMaps: true,
-  webpack: (config, {isServer}) => {
-    if (!isServer) {
-      config.resolve.alias['@sentry/node'] = '@sentry/browser'
-    }
-    return config
-  }
 }
