fc3caddfac19c2bc1b641fa0d9000087f3af5f46
Sarath M S <sarath.ms@gmail.com>
2021-10-16 04:11:30 -0400

    Configure Sentry with @sentry/nextjs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /* global module */
-module.exports = {
-  productionBrowserSourceMaps: true,
+
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+const { withSentryConfig } = require('@sentry/nextjs')
+
+const SentryWebpackPluginOptions = {
+  // https://github.com/getsentry/sentry-webpack-plugin#options
+  silent: true, // Suppresses all logs
 }
+
+module.exports = withSentryConfig({
+  productionBrowserSourceMaps: true,
+}, SentryWebpackPluginOptions)
