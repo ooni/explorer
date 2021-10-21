3b6f6ddebe8e9b439e1ea24e234be5d952325fb4
Sarath <sarath.ms@gmail.com>
2021-10-21 13:39:32 -0400

    Fix MAT tooltip link to search page (#618)
    Add link to search for individual measurements shown in MAT charts

--- components/aggregation/mat/CustomTooltip.js
+++ components/aggregation/mat/CustomTooltip.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { Chip } from '@nivo/tooltip'
 import { useTheme } from '@nivo/core'
 import { Box, Flex, Text, Link } from 'ooni-components'
@@ -6,10 +6,41 @@ import NLink from 'next/link'
 
 import { colorMap } from './colorMap'
 import { MdClear } from 'react-icons/md'
+import { useRouter } from 'next/router'
+
+
+const urlToDomain = (url) => new URL(url).hostname
 
 const CustomToolTip = React.memo(({ data, onClose }) => {
   const theme = useTheme()
+  const { query: { probe_cc, axis_y } } = useRouter()
   const dataKeysToShow = ['anomaly_count', 'confirmed_count', 'failure_count', 'ok_count']
+
+  const linkToMeasurements = useMemo(() => {
+
+    const untilDate = new Date(Date.parse(data.measurement_start_day))
+    const untilPlus1 = new Date(untilDate)
+    untilPlus1.setUTCDate(untilDate.getUTCDate() + 1)
+    const nextDay = untilPlus1.toISOString().split('T')[0]
+
+    const yAxisFilter = axis_y === 'input' ? (
+      { domain: urlToDomain(data[axis_y]) }
+    ) : (
+      { [axis_y]: data[axis_y] }
+    )
+
+    let urlObj = {
+      pathname: '/search',
+      query: {
+        probe_cc,
+        test_name: 'web_connectivity',
+        since: data.measurement_start_day,
+        until: nextDay,
+        ...yAxisFilter
+      }
+    }
+    return urlObj
+  }, [axis_y, data, probe_cc])
   
   return (
     <Flex flexDirection='column' style={theme.tooltip.container}>
@@ -28,7 +59,7 @@ const CustomToolTip = React.memo(({ data, onClose }) => {
           </Box>
         ))}
       </Flex>
-      <Link my={2} ml='auto' pr={3}><NLink href='/search'>view measurements &gt;</NLink></Link>
+      <NLink passHref href={linkToMeasurements}><Link my={2} ml='auto' pr={3}>view measurements &gt;</Link></NLink>
     </Flex>
   )
 })
