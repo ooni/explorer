bc490ea8d1c68b4a8f9f1205e12950484f15b18b
Sarath M S <sarath.ms@gmail.com>
2021-11-09 21:04:36 -0500

    Restore showing errors in Explorer search results

--- components/colors.js
+++ components/colors.js
@@ -1,7 +1,7 @@
 import { theme } from 'ooni-components'
 
 export const colorNormal = theme.colors.green7
-export const colorError = theme.colors.yellow5
+export const colorError = theme.colors.gray4
 export const colorConfirmed = theme.colors.red8
 export const colorAnomaly = theme.colors.yellow8
 export const colorEmpty = theme.colors.gray3
