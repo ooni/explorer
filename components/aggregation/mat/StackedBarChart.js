77f51b6de00074c8cffbf6a87eddc0271f42158d
Sarath M S <sarath.ms@gmail.com>
2022-01-20 10:01:00 -0500

    Add OONI logo to 1-dimension chart

--- components/aggregation/mat/StackedBarChart.js
+++ components/aggregation/mat/StackedBarChart.js
@@ -5,6 +5,7 @@ import { Box, Flex, Link } from 'ooni-components'
 import { IoMdGlobe } from 'react-icons/io'
 import NLink from 'next/link'
 import { useIntl } from 'react-intl'
+import OONILogo from 'ooni-components/components/svgs/logos/OONI-HorizontalMonochrome.svg'
 
 import { colorMap } from './colorMap'
 import { generateSearchQuery, CustomTooltipNoLink} from './CustomTooltip'
@@ -58,82 +59,86 @@ export const StackedBarChart = ({ data, query }) => {
   }
 
   return (
-    <Flex flexDirection={['column']} height={'100%'}>
-    <Box alignSelf={'flex-end'}>
-      {link ? (
-      <Flex alignItems='center'>
-        <Box mx={1}>
-          <IoMdGlobe size={18} />
-        </Box>
-        <Box>
-          {link}
-        </Box>
-      </Flex>
-      ): (
-        <Box> Click on a bar to explore the measurements aggregated in the column </Box>
-      )}
-    </Box>
-    <Box height={'100%'}>
-    <ResponsiveBar
-      data={chartMeta.data}
-      keys={chartMeta.cols}
-      indexBy={chartMeta.indexBy}
-      margin={{ top: 50, right: 130, bottom: 100, left: 80 }}
-      padding={0.3}
-      colors={colorFunc}
-      borderColor={{ from: 'color', modifiers: [ [ 'darker', 1.6 ] ] }}
-      axisTop={null}
-      axisRight={null}
-      axisBottom={{
-        tickSize: 5,
-        tickPadding: 5,
-        tickRotation: 45,
-        legend: `${chartMeta.indexBy}`,
-        legendPosition: 'middle',
-        legendOffset: 60
-      }}
-      axisLeft={{
-        tickSize: 5,
-        tickPadding: 5,
-        tickRotation: 0,
-        legend: 'measurement count',
-        legendPosition: 'middle',
-        legendOffset: -60
-      }}
-      labelSkipWidth={80}
-      labelSkipHeight={20}
-      labelTextColor={{ from: 'color', modifiers: [ [ 'darker', 1.6 ] ] }}
-      legends={[
-        {
-          dataFrom: 'keys',
-          anchor: 'bottom',
-          direction: 'row',
-          justify: false,
-          translateX: 0,
-          translateY: 100,
-          itemsSpacing: 2,
-          itemWidth: 200,
-          itemHeight: 20,
-          itemDirection: 'left-to-right',
-          itemOpacity: 0.85,
-          onClick: (d, e) => { alert(`Toggle ${JSON.stringify(d)}`)},
-          effects: [
+    <Flex flexDirection={['column']} height={'100%'} sx={{ position: 'relative' }}>
+      <Box alignSelf={'flex-end'}>
+        {link ? (
+        <Flex alignItems='center'>
+          <Box mx={1}>
+            <IoMdGlobe size={18} />
+          </Box>
+          <Box>
+            {link}
+          </Box>
+        </Flex>
+        ): (
+          <div />
+          // <Box> Click on a bar to explore the measurements aggregated in the column </Box>
+        )}
+      </Box>
+      <Box height={'100%'}>
+        <ResponsiveBar
+          data={chartMeta.data}
+          keys={chartMeta.cols}
+          indexBy={chartMeta.indexBy}
+          margin={{ top: 50, right: 80, bottom: 100, left: 80 }}
+          padding={0.3}
+          colors={colorFunc}
+          borderColor={{ from: 'color', modifiers: [ [ 'darker', 1.6 ] ] }}
+          axisTop={null}
+          axisRight={null}
+          axisBottom={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 45,
+            legend: `${chartMeta.indexBy}`,
+            legendPosition: 'middle',
+            legendOffset: 60
+          }}
+          axisLeft={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: 'measurement count',
+            legendPosition: 'middle',
+            legendOffset: -60
+          }}
+          labelSkipWidth={80}
+          labelSkipHeight={20}
+          labelTextColor={{ from: 'color', modifiers: [ [ 'darker', 1.6 ] ] }}
+          legends={[
             {
-              on: 'hover',
-              style: {
-                itemOpacity: 1
-              }
+              dataFrom: 'keys',
+              anchor: 'bottom',
+              direction: 'row',
+              justify: false,
+              translateX: 0,
+              translateY: 100,
+              itemsSpacing: 2,
+              itemWidth: 200,
+              itemHeight: 20,
+              itemDirection: 'left-to-right',
+              itemOpacity: 0.85,
+              onClick: (d, e) => { alert(`Toggle ${JSON.stringify(d)}`)},
+              effects: [
+                {
+                  on: 'hover',
+                  style: {
+                    itemOpacity: 1
+                  }
+                }
+              ]
             }
-          ]
-        }
-      ]}
-      animate={true}
-      motionStiffness={90}
-      motionDamping={15}
-      tooltip={CustomTooltipNoLink}
-      onClick={onClick}
-    />
-    </Box>
+          ]}
+          animate={true}
+          motionStiffness={90}
+          motionDamping={15}
+          tooltip={CustomTooltipNoLink}
+          onClick={onClick}
+        />
+      </Box>
+      <Box alignSelf='flex-end' sx={{ position: 'absolute', bottom: -10, right: 10 }}>
+        <OONILogo height='32px' />
+      </Box>
     </Flex>
   )
 }
