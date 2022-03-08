85a016bccb1e9a2b45605fdd63b959783f402f7d
Sarath M S <sarath.ms@gmail.com>
2022-03-08 13:25:21 -0500

    Filter charts using table row selection

--- components/aggregation/mat/TableView.js
+++ components/aggregation/mat/TableView.js
@@ -2,15 +2,15 @@ import React, { useCallback, useMemo, useState } from 'react'
 import { useTable, useFlexLayout, useRowSelect, useFilters, useGroupBy, useSortBy } from 'react-table'
 import { FormattedMessage, useIntl } from 'react-intl'
 import styled from 'styled-components'
-import { Flex, Box } from 'ooni-components'
+import { Flex, Box, Button, Text } from 'ooni-components'
 
 import GridChart from './GridChart'
 import { useDebugContext } from '../DebugContext'
 import { getRowLabel } from './labels'
 import { ResizableBox } from './Resizable'
+import { DetailsBox } from '../../measurement/DetailsBox'
 
 const TableContainer = styled.div`
-  padding: 1rem;
   ${'' /* These styles are suggested for the table fill all available space in its containing element */}
   flex: 1;
   ${'' /* These styles are required for a horizontaly scrollable table overflow */}
@@ -19,38 +19,43 @@ const TableContainer = styled.div`
 
 const Table = styled.div`
   border-spacing: 0;
+  border: 1px solid black;
 `
 
 const Cell = styled.div`
-  /* margin: 0;
-  padding: 1rem; */
-
-  ${'' /* In this example we use an absolutely position resizer,
-    so this is required. */}
-  /* position: relative; */
-
-  &:last-child {
-    border-right: 0;
-  }
+  padding: 8px;
 `
 
 const TableRow = styled(Flex)`
-  &:last-child {
-    ${Cell} {
-      border-bottom: 0;
-    }
-  }
   border-bottom: 1px solid black;
+  &:last-child {
+    border-bottom: 0;
+  }
+  &:hover {
+    background: ${props => props.theme.colors.gray1};
+  }
 `
 
 const TableHeader = styled.div`
-  ${'' /* These styles are required for a scrollable body to align with the header properly */}
-  overflow-y: auto;
-  overflow-x: hidden;
   ${TableRow} {
-    padding-bottom: 8px;
-    border-bottom: 1px solid black;
+    margin-bottom: 8px;
+    border-bottom: 2px solid black;
   }
+  & ${Cell} {
+    border-right: 1px solid black;
+    font-weight: bold;
+    &:last-child {
+      border-right: 0;
+    }
+  }
+
+`
+
+const TableBody = styled.div`
+  ${'' /* These styles are required for a scrollable table body */}
+  overflow-y: scroll;
+  overflow-x: hidden;
+  height: 250px;
 `
 
 const IndeterminateCheckbox = React.forwardRef(
@@ -105,7 +110,7 @@ const TableView = ({ data, query }) => {
   const defaultColumn = React.useMemo(
     () => ({
       // When using the useFlexLayout:
-      width: 200, // width is used for both the flex-basis and flex-grow
+      width: 70, // width is used for both the flex-basis and flex-grow
       Filter: SearchFilter,
     }),
     []
@@ -137,10 +142,18 @@ const TableView = ({ data, query }) => {
   const columns = useMemo(() => [
     {
       Header: intl.formatMessage({ id: `MAT.Table.Header.${yAxis}`}),
+      Cell: ({ value, row, toggleRowSelected }) => (
+        <Text fontWeight={row.isSelected ? 'bold' : 'initial'}>
+          {value}
+        </Text>
+      ),
       id: 'yAxisLabel',
       accessor: 'rowLabel',
       aggregate: (values) => values[0],
       filter: 'text',
+      style: {
+        width: '45%'
+      }
     },
     {
       id: 'yAxisCode',
@@ -152,24 +165,36 @@ const TableView = ({ data, query }) => {
       accessor: 'anomaly_count',
       aggregate: 'sum',
       disableFilters: true,
+      style: {
+        textAlign: 'end'
+      }
     },
     {
       Header: <FormattedMessage id='MAT.Table.Header.confirmed_count' />,
       accessor: 'confirmed_count',
       aggregate: 'sum',
       disableFilters: true,
+      style: {
+        textAlign: 'end'
+      }
     },
     {
       Header: <FormattedMessage id='MAT.Table.Header.failure_count' />,
       accessor: 'failure_count',
       aggregate: 'sum',
       disableFilters: true,
+      style: {
+        textAlign: 'end'
+      }
     },
     {
       Header: <FormattedMessage id='MAT.Table.Header.measurement_count' />,
       accessor: 'measurement_count',
       aggregate: 'sum',
       disableFilters: true,
+      style: {
+        textAlign: 'end'
+      }
     }
   ], [intl, yAxis])
 
@@ -183,12 +208,8 @@ const TableView = ({ data, query }) => {
     getTableBodyProps,
     headerGroups,
     rows, // contains filtered rows
-    preFilteredRows,
+    toggleAllRowsSelected,
     selectedFlatRows,
-    filteredFlatRows,
-    groupedFlatRows,
-    onlyGroupedFlatRows,
-    nonGroupedFlatRows,
     prepareRow
   } = useTable(
     {
@@ -208,7 +229,7 @@ const TableView = ({ data, query }) => {
         // Let's make a column for selection
         {
           id: 'selection',
-          width: 50,
+          width: 30,
           // The header can use the table's getToggleAllRowsSelectedProps method
           // to render a checkbox
           // eslint-disable-next-line react/display-name
@@ -231,14 +252,6 @@ const TableView = ({ data, query }) => {
     }
   )
 
-  const dataForCharts = useMemo(() => {
-    if (selectedFlatRows.length > 0) {
-      return selectedFlatRows
-    }
-
-    return rows
-  }, [rows, selectedFlatRows])
-
   const [chartPanelHeight, setChartPanelHeight] = useState(800)
 
   const onPanelResize = useCallback((width, height) => {
@@ -246,8 +259,85 @@ const TableView = ({ data, query }) => {
     setChartPanelHeight(height - (90 + 62))
   }, [])
 
+  const [dataForCharts, setDataForCharts] = useState(rows)
+  
+  const updateCharts = useCallback(() => {
+    if (selectedFlatRows.length > 0 && selectedFlatRows.length < rows.length) {
+      setDataForCharts(selectedFlatRows)
+      setChartPanelHeight(selectedFlatRows.length * 70)
+    } else {
+      setDataForCharts(rows)
+      setChartPanelHeight('auto')
+    }
+  }, [rows, selectedFlatRows])
+
+  const showChartsButton = selectedFlatRows.length > 0 && selectedFlatRows.length < rows.length ? (
+    <Button hollow onClick={updateCharts}>Show {selectedFlatRows.length} Charts</Button>
+  ) : (
+    <Text>Select rows to show</Text>      
+  )
+
+  const resetFilter = () => {
+    toggleAllRowsSelected(false)
+    setDataForCharts(rows)
+    setChartPanelHeight('auto')
+  }
+
   return (
     <Flex flexDirection='column'>
+      <DetailsBox title={'Filters'} collapsed={false}>
+        <Flex flexDirection='column'>
+          <Flex my={2} alignItems='center'>
+            {showChartsButton}
+            <Button inverted onClick={resetFilter} mx={3} disabled={selectedFlatRows.length === 0}>Reset</Button>
+          </Flex>
+          <TableContainer>
+            {/* eslint-disable react/jsx-key */}
+            <Table {...getTableProps()}>
+              <TableHeader>
+                {headerGroups.map(headerGroup => (
+                  <TableRow {...headerGroup.getHeaderGroupProps()}>
+                    {headerGroup.headers.map(column => {
+                      return (
+                        <Cell {...column.getHeaderProps([
+                          {
+                            style: column.style
+                          }
+                        ])}>
+                          {column.render('Header')}
+                          {/* Column Filter */}
+                          <div>{column.canFilter ? column.render('Filter') : null}</div>
+                        </Cell>
+                      )}
+                    )}
+                  </TableRow>
+                ))}
+              </TableHeader>
+              <TableBody {...getTableBodyProps()}>
+                {rows.map(row => {
+                  prepareRow(row)
+                  return (
+                    <TableRow {...row.getRowProps()}>
+                      {row.cells.map(cell => {
+                        return (
+                          <Cell {...cell.getCellProps([
+                            {
+                              style: cell.column.style
+                            }
+                          ])}>
+                            {cell.render('Cell')}
+                          </Cell>
+                        )
+                      })}
+                    </TableRow>
+                  )
+                })}
+              </TableBody>
+            </Table>
+            {/* eslint-enable react/jsx-key */}
+          </TableContainer>
+        </Flex>
+      </DetailsBox>
       <ResizableBox onResize={onPanelResize}>
         <GridChart
           data={dataForCharts}
@@ -256,49 +346,6 @@ const TableView = ({ data, query }) => {
           height={chartPanelHeight}
         />
       </ResizableBox>
-      <Flex my={4} sx={{ height: '50vh' }}>
-        <TableContainer>
-          {/* eslint-disable react/jsx-key */}
-          <Table {...getTableProps()}>
-            <TableHeader>
-              {headerGroups.map(headerGroup => (
-                <TableRow {...headerGroup.getHeaderGroupProps()}>
-                  {headerGroup.headers.map(column => {
-                    return (
-                      <Cell
-                        {...column.getHeaderProps()}
-                      >
-                        {column.render('Header')}
-                        {/* Column Filter */}
-                        <div>{column.canFilter ? column.render('Filter') : null}</div>
-                      </Cell>
-                    )}
-                  )}
-                </TableRow>
-              ))}
-            </TableHeader>
-            <div {...getTableBodyProps()}>
-              {rows.map(row => {
-                prepareRow(row)
-                return (
-                  <TableRow {...row.getRowProps()}>
-                    {row.cells.map(cell => {
-                      return (
-                        <Cell
-                          {...cell.getCellProps()}
-                        >
-                          {cell.render('Cell')}
-                        </Cell>
-                      )
-                    })}
-                  </TableRow>
-                )
-              })}
-            </div>
-          </Table>
-          {/* eslint-enable react/jsx-key */}
-        </TableContainer>
-      </Flex>
     </Flex>
   )
 }
