702c109c9cdce8cee00af9191b6fe8bd462f20b6
Sarath M S <sarath.ms@gmail.com>
2022-03-30 07:39:58 -0400

    Use smaller heading for subtitle

--- pages/chart/mat.js
+++ pages/chart/mat.js
@@ -98,7 +98,7 @@ const MeasurementAggregationToolkit = ({ testNames }) => {
         <Container>
           <Flex flexDirection='column'>
             <Heading h={1} mt={3} mb={0}><FormattedMessage id='MAT.Title' /></Heading>
-            <Heading h={3} mt={0} mb={2}><FormattedMarkdown id='MAT.SubTitle' /></Heading>
+            <Heading h={5} mt={0} mb={2}><FormattedMessage id='MAT.SubTitle' /></Heading>
             <Form onSubmit={onSubmit} testNames={testNames} query={router.query} />
             <Box sx={{ }}>
               {showLoadingIndicator &&
