a931a6c16cdedf6c06c57684f040ccd9f6e3d5ce
GermaVinsmoke <vaibhav1180@gmail.com>
2022-04-01 00:29:49 +0530

    replaced custom date format function with dayjs
    Signed-off-by: GermaVinsmoke <vaibhav1180@gmail.com>

--- utils/dataExport.js
+++ utils/dataExport.js
@@ -1,16 +1,11 @@
 import axios from 'axios'
+import dayjs from 'services/dayjs'
 
 export const requestUrl = (query) => {
   const qs = new URLSearchParams(query).toString()
   const reqUrl = `${process.env.NEXT_PUBLIC_AGGREGATION_API || process.env.NEXT_PUBLIC_MEASUREMENTS_URL}/api/v1/aggregation?${qs}&format=CSV`
 
   return reqUrl;
-}
-
-const getFormattedDate = () => {
-  const now = new Date();
-
-  return `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`
 }
 
 export const exportToCsv = async (event, query) => {
@@ -21,7 +16,7 @@ export const exportToCsv = async (event, query) => {
     const response = await axios.get(url)
     const element = document.createElement('a')
     element.href = 'data:text/csv;charset=utf-8,' + response.data
-    element.download = `${getFormattedDate()}-ooni-data.csv`
+    element.download = `${dayjs().format('YYYY-MM-DD')}-ooni-data.csv`
     element.click()
   } catch (error) {
     console.log(error)
@@ -32,6 +27,6 @@ export const exportToJson = (event, jsonContent) => {
   event.preventDefault();
   const element = document.createElement('a')
   element.href = 'data:text/json;charset=utf-8,' + JSON.stringify(jsonContent, null, '\t')
-  element.download = `${getFormattedDate()}-ooni-data.json`
+  element.download = `${dayjs().format('YYYY-MM-DD')}-ooni-data.json`
   element.click()
 }
