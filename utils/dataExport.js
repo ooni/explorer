652cc230320b764bcf5a5ad92a6bb87d9e0e2cb7
GermaVinsmoke <vaibhav1180@gmail.com>
2022-04-01 22:19:08 +0530

    removed semi colons

--- utils/dataExport.js
+++ utils/dataExport.js
@@ -5,12 +5,12 @@ export const requestUrl = (query) => {
   const qs = new URLSearchParams(query).toString()
   const reqUrl = `${process.env.NEXT_PUBLIC_AGGREGATION_API || process.env.NEXT_PUBLIC_MEASUREMENTS_URL}/api/v1/aggregation?${qs}&format=CSV`
 
-  return reqUrl;
+  return reqUrl
 }
 
 export const exportToCsv = async (event, query) => {
-  event.preventDefault();
-  const url = requestUrl(query);
+  event.preventDefault()
+  const url = requestUrl(query)
 
   try {
     const response = await axios.get(url)
@@ -24,7 +24,7 @@ export const exportToCsv = async (event, query) => {
 }
 
 export const exportToJson = (event, jsonContent) => {
-  event.preventDefault();
+  event.preventDefault()
   const element = document.createElement('a')
   element.href = 'data:text/json;charset=utf-8,' + JSON.stringify(jsonContent, null, '\t')
   element.download = `${dayjs().format('YYYY-MM-DD')}-ooni-data.json`
