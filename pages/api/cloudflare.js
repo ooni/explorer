7d6ba25776cdabd919b3d2963ec0b52360719a37
Maja Komel <maja.komel@gmail.com>
2023-01-12 13:54:53 +0100

    Add third party data to country pages

---
+++ pages/api/cloudflare.js
@@ -0,0 +1,40 @@
+import axios from 'axios'
+import dayjs from 'services/dayjs'
+
+export default function handler(req, res) {
+  const dateStart = req.query?.from
+  const dateEnd = req.query?.to
+  const location = req.query?.country
+
+  const diff = dayjs(dateEnd).diff(dayjs(dateStart), 'day')
+  const aggInterval = diff <= 31 ? '1h' : '1d'
+
+  axios({
+    method:'get',
+    url:`https://api.cloudflare.com/client/v4/radar/netflows/timeseries?name=all&product=all&dateStart=${dateStart}&dateEnd=${dateEnd}&location=${location}&name=http&product=http&dateStart=${dateStart}&dateEnd=${dateEnd}&location=${location}&aggInterval=${aggInterval}&normalization=MIN0_MAX`,
+
+    
+    
+    // params: {
+    //   aggInterval: '1h',
+    //   dateRange: '7d',
+    //   location: 'RU',
+    //   // product: ['http', 'all'],
+    //   name: 'bla'
+    // },
+    // paramsSerializer: params => {
+    //   return qs.stringify(params)
+    // },
+    // baseURL: 'https://api.cloudflare.com/',
+    headers: {
+      'X-Auth-Key': process.env.CLOUDFLARE_TOKEN,
+      'X-Auth-Email': process.env.CLOUDFLARE_EMAIL
+    },
+   }).then(({data}) => {
+    console.log('cloudflareData', data)
+    return res.status(200).json(data)
+  }).catch((err) =>{
+    console.log('error', err.response.data)
+    return res.status(200).json(err)
+  }) 
+}
--- pages/country/[countryCode].js
+++ pages/country/[countryCode].js
@@ -12,6 +12,7 @@ import { useIntl } from 'react-intl'
 import { StickyContainer, Sticky } from 'react-sticky'
 import { getLocalisedRegionName } from '../../utils/i18nCountries'
 import dayjs from 'services/dayjs'
+import { ResponsiveLine } from '@nivo/line'
 
 import Form from 'components/network/Form'
 import NavBar from 'components/NavBar'
@@ -138,10 +139,62 @@ const Country = ({ countryCode, overviewStats, reports, ...coverageDataSSR }) =>
     ) {
       router.push(href, href, { shallow: true })
     }
-
   }
 
   const { testCoverage, networkCoverage } = newData !== false ? newData : coverageDataSSR
+
+  const [graphData, setGraphData] = useState([])
+
+  const cloudflareData = useEffect(() => {
+    if (query.until && query.until) {
+      const to = dayjs.utc(query.until)
+      const from = dayjs.utc(query.since)
+
+      axios({
+        method: 'get',
+        url: `/api/cloudflare?from=${from.toISOString().split('.')[0]+'Z'}&to=${to.toISOString().split('.')[0]+'Z'}&country=${countryCode}`,
+        baseURL: 'http://localhost:3100',
+      }).then(({data}) => {
+        const ruData = data.result.all.timestamps.map((st, i) => {
+          return {
+            'x': st,
+            'y': Number(data.result.all.values[i])
+          }
+        })
+        setGraphData((oldVal) => {
+          return [...oldVal, {
+            'id': 'Cloudflare',
+            // 'color': 'hsl(0, 100%, 50%)',
+            'data': ruData,
+          }]
+        })
+      }).catch(() => {})
+  
+      axios({
+          method: 'get',
+          url: `https://api.ioda.inetintel.cc.gatech.edu/v2/signals/raw/country/${countryCode}?from=${Math.round(from.valueOf()/1000)}&until=${Math.round(to.valueOf()/1000)}&sourceParams=WEB_SEARCH`,
+      }).then(({data}) => {
+        const graphData2 = data.data[0].map((item) => {
+          const max = Math.max(...item.values)
+          const values = item.values.map((val, i) => {
+            const time = dayjs(item.from * 1000).add(item.step * i, 'second').utc().toISOString().split('.')[0]+'Z'
+            return {x: time, y: val ? val / max : null}
+          })
+          return {
+            'id': item.datasource,
+            // 'color': 'hsl(100, 70%, 50%)',
+            'data': values
+          }
+        })
+  
+        setGraphData((oldVal) => {
+          return [...oldVal, ...graphData2]
+        })
+      })
+    }
+  }, [])
+
+  console.log('graphData', graphData)
 
   return (
     <>
@@ -195,6 +248,107 @@ const Country = ({ countryCode, overviewStats, reports, ...coverageDataSSR }) =>
                 <WebsitesSection countryCode={countryCode} />
                 <AppsSection />
               </CountryContextProvider>
+            </Box>
+            <Box style={{width: '100%',height: '500px'}}>
+              {!!graphData.length && 
+                  <ResponsiveLine
+                  data={graphData}
+                  margin={{ top: 50, right: 20, bottom: 70, left: 30 }}
+                  enablePoints={false}
+                  lineWidth={1}
+                  xScale={{
+                    type: 'time',
+                    format: '%Y-%m-%dT%H:%M:%SZ',
+                    precision: 'minute',
+                    useUTC: true,
+                  }}
+                  yScale={{
+                    type: 'linear',
+                    stacked: false,
+                    min: 0,
+                    max: 1
+                  }}
+                  // xFormat="time:%Hh"
+                  axisBottom={{
+                    format: '%Y-%m-%d',
+                  }}
+                  
+
+
+
+                  // data={graphData}
+                  // curve="monotoneX"
+                  // enableSlices='x'
+                  // animate={true}
+                  // margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+                  // xScale={{ 
+                  //   type: 'time',
+                  //   format: '%Y-%m-%d',
+                  //   useUTC: true,
+                  //   precision: 'day',
+                  // }}
+                  // yScale={{
+                  //     type: 'linear',
+                  //     min: 'auto',
+                  //     max: 'auto',
+                  //     stacked: false,
+                  //     reverse: false
+                  // }}
+                  // // yFormat=" >-.2f"
+                  // // axisTop={null}
+                  // // axisRight={null}
+                  // axisBottom={{
+                  //   format: '%Y-%m-%d',
+                  //   orient: 'bottom',
+                  //   tickSize: 5,
+                  //   tickPadding: 5,
+                  //   tickRotation: 0,
+                  //   legend: 'date',
+                  //   legendOffset: 36,
+                  //   legendPosition: 'middle',
+                  //   tickValues: 6
+                  // }}
+                  // axisLeft={{
+                  //     orient: 'left',
+                  //     tickSize: 5,
+                  //     tickPadding: 5,
+                  //     tickRotation: 0,
+                  //     legend: 'count',
+                  //     legendOffset: -40,
+                  //     legendPosition: 'middle'
+                  // }}
+                  // pointSize={10}
+                  // pointColor={{ theme: 'background' }}
+                  // pointBorderWidth={2}
+                  // pointBorderColor={{ from: 'serieColor' }}
+                  // pointLabelYOffset={-12}
+                  useMesh={true}
+                  legends={[
+                    {
+                      anchor: 'bottom-left',
+                      direction: 'row',
+                      // justify: false,
+                      // translateX: 100,
+                      translateY: 70,
+                      itemsSpacing: 0,
+                      itemDirection: 'left-to-right',
+                      itemWidth: 90,
+                      itemHeight: 20,
+                      // itemOpacity: 0.75,
+                      symbolSize: 12,
+                      symbolShape: 'circle',
+                      symbolBorderColor: 'rgba(0, 0, 0, .5)',
+                      // effects: [{
+                      //   on: 'hover',
+                      //   style: {
+                      //     itemBackground: 'rgba(0, 0, 0, .03)',
+                      //     itemOpacity: 1
+                      //   }
+                      // }]
+                    }
+                  ]}
+                />
+              }
             </Box>
           </Flex>
         </Container>
