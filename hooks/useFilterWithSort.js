0cc5176325eb1497ba65bc63f1dbb11d7d9fc5ed
Maja Komel <maja.komel@gmail.com>
2023-03-28 15:17:46 +0200

    Add domains and networks pages

---
+++ hooks/useFilterWithSort.js
@@ -0,0 +1,25 @@
+import { useMemo, useState } from 'react'
+import debounce from 'lodash.debounce'
+
+const useFilterWithSort = ({ initialSortValue, initialFilterValue = '' }) => {
+  const [searchValue, setSearchValue] = useState('')
+  const [categoryValue, setCategoryValue] = useState('')
+  const [sortValue, setSortValue] = useState(initialSortValue)
+  
+  const searchHandler = (searchTerm) => {
+    setSearchValue(searchTerm)
+  }
+
+  const debouncedSearchHandler = useMemo(() => debounce(searchHandler, 200),[])
+  
+  return {
+    searchValue,
+    sortValue,
+    setSortValue,
+    categoryValue,
+    setCategoryValue,
+    debouncedSearchHandler,
+  }
+}
+
+export default useFilterWithSort
