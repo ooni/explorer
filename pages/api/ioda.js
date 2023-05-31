cf09c5aa36268602fb9e8317e0e257a70877d29f
Maja Komel <maja.komel@gmail.com>
2023-05-31 10:25:11 +0200

    Include only predefined ioda datasources

--- pages/api/ioda.js
+++ pages/api/ioda.js
@@ -24,11 +24,15 @@ const iodaHandler = (req, res) => {
   const formattedFrom = Math.round(dayjs(dateStart).utc().valueOf()/1000)
   const formattedTo = Math.round(dayjs(dateEnd).utc().valueOf()/1000)
 
+  const datasources = [ 'gtr', 'merit-nt', 'bgp', 'ping-slash24' ]
+
   return axios({
     method: 'get',
     url: `http://api.ioda.inetintel.cc.gatech.edu/v2/signals/raw/${country ? 'country' : 'asn'}/${location}?from=${formattedFrom}&until=${formattedTo}&sourceParams=WEB_SEARCH`,
   }).then(({data}) => {
-    const result = data.data[0].map((item, i) => {
+    const result = data.data[0]
+    .filter((item) => datasources.includes(item.datasource))
+    .map((item, i) => {
       const max = Math.max(...item.values)
       const values = item.values.map((val, i) => {
         const time = dayjs(item.from * 1000).add(item.step * i, 'second').utc().toISOString().split('.')[0]+'Z'
