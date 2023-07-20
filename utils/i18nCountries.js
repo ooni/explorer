b082f550fd7fc54e2e2ccde8daa4993f29ce84a5
majakomel <maja.komel@gmail.com>
2023-07-20 10:53:47 +0200

    Update translations, add sw and zh-hant

--- utils/i18nCountries.js
+++ utils/i18nCountries.js
@@ -2,7 +2,7 @@ import { countryList } from 'country-util'
 import '@formatjs/intl-displaynames/polyfill'
 
 const getLocale = (locale) => {
-  return locale === 'zh-cn' ? 'zh-Hans' : locale
+  return locale === 'zh-CN' ? 'zh-Hans' : locale
 }
 // eventually we can remove this, but currently Chrome doesn't have the translations for UN M.49 area codes implemented so we need to polyfill
 process.env.LOCALES.forEach((locale) => {
@@ -15,26 +15,29 @@ export const getLocalisedRegionName = (regionCode, locale) => {
   locale = getLocale(locale)
 
   try {
-    return new Intl.DisplayNames([locale], { type: 'region' }).of(String(regionCode))
+    return new Intl.DisplayNames([locale], { type: 'region' }).of(
+      String(regionCode)
+    )
   } catch (e) {
     return regionCode
-  } 
+  }
 }
 
 export const getLocalisedLanguageName = (regionCode, locale) => {
   locale = getLocale(locale)
 
   try {
-    return new Intl.DisplayNames([locale], { type: 'language' }).of(String(regionCode))
+    return new Intl.DisplayNames([locale], { type: 'language' }).of(
+      String(regionCode)
+    )
   } catch (e) {
     return regionCode
-  } 
+  }
 }
 
 export const localisedCountries = (locale) => {
   return countryList.map((c) => ({
-      ...c,
-      localisedCountryName: getLocalisedRegionName(c.iso3166_alpha2, locale)
-    })
-  )
+    ...c,
+    localisedCountryName: getLocalisedRegionName(c.iso3166_alpha2, locale),
+  }))
 }
