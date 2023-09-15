7f0117ef13aca90e535329d5785fc83a8be54fc1
majakomel <maja.komel@gmail.com>
2023-09-15 14:14:36 +0200

    cleanup

--- cypress/mocks/browser.js
+++
@@ -1,8 +0,0 @@
-// src/mocks/browser.js
-import { setupWorker, rest } from 'msw'
-import { handlers } from './handlers'
-
-// This configures a Service Worker with the given request handlers.
-const worker = setupWorker(...handlers)
-
-export { worker, rest }
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -16,7 +16,6 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 import './msw'
-// import { worker, rest } from '../mocks/browser'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
