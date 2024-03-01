4569a4e99bdabb8b110955fd53f4aa30129a9236
majakomel <maja.komel@gmail.com>
2024-03-01 14:03:14 +0100

    Update Sentry config

--- next.config.js
+++ next.config.js
@@ -7,13 +7,6 @@ const glob = require('glob')
 const { dirname, basename, resolve } = require('path')
 const { execSync } = require('child_process')
 
-const SentryWebpackPluginOptions = {
-  // https://github.com/getsentry/sentry-webpack-plugin#options
-  debug: process.env.NODE_ENV === 'development',
-  dryRun: process.env.NODE_ENV === 'development',
-  release: process.env.GIT_COMMIT_SHA,
-  silent: false,
-}
 
 const LANG_DIR = './public/static/lang/'
 const DEFAULT_LOCALE = 'en'
@@ -27,96 +20,124 @@ function getSupportedLanguages() {
   return [...supportedLanguages]
 }
 
-module.exports = withSentryConfig({
-  output: 'standalone',
-  async redirects() {
-    return [
-      {
-        source: '/experimental/mat',
-        destination: '/chart/mat',
-        permanent: true,
-      },
-    ]
-  },
-  compiler: {
-    // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
-    styledComponents: {
-      ssr: true,
+module.exports = withSentryConfig(
+  {
+    output: 'standalone',
+    async redirects() {
+      return [
+        {
+          source: '/experimental/mat',
+          destination: '/chart/mat',
+          permanent: true,
+        },
+      ]
     },
-  },
-  i18n: {
-    locales: getSupportedLanguages(),
-    defaultLocale: DEFAULT_LOCALE,
-  },
-  webpack: (config, options) => {
-    const gitCommitSHAShort = process.env.RUN_GIT_COMMIT_SHA_SHORT ? execSync(process.env.RUN_GIT_COMMIT_SHA_SHORT) : ''
-    const gitCommitSHA = process.env.RUN_GIT_COMMIT_SHA ? execSync(process.env.RUN_GIT_COMMIT_SHA) : ''
-    const gitCommitRef = process.env.RUN_GIT_COMMIT_REF ? execSync(process.env.RUN_GIT_COMMIT_REF) : ''
-    const gitCommitTags = process.env.RUN_GIT_COMMIT_TAGS ? execSync(process.env.RUN_GIT_COMMIT_TAGS) : ''
-
-    config.plugins.push(
-      new options.webpack.DefinePlugin({
-        'process.env.GIT_COMMIT_SHA_SHORT': JSON.stringify(
-          gitCommitSHAShort.toString()
-        ),
-        'process.env.GIT_COMMIT_SHA': JSON.stringify(gitCommitSHA.toString()),
-        'process.env.GIT_COMMIT_REF': JSON.stringify(gitCommitRef.toString()),
-        'process.env.GIT_COMMIT_TAGS': JSON.stringify(
-          gitCommitTags.toString()
-        ),
-        'process.env.DEFAULT_LOCALE': DEFAULT_LOCALE,
-        'process.env.LOCALES': JSON.stringify(getSupportedLanguages()),
-        'process.env.WDYR': JSON.stringify(process.env.WDYR),
-      })
-    )
-    
-    // SVG
-    // Grab the existing rule that handles SVG imports
-    const fileLoaderRule = config.module.rules.find((rule) =>
-      rule.test?.test?.('.svg'),
-    )
-
-    config.module.rules.push(
-      // Convert all *.svg imports to React components
-      {
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        use: ['@svgr/webpack'],
+    compiler: {
+      // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
+      styledComponents: {
+        ssr: true,
       },
-    )
-
-    // Modify the file loader rule to ignore *.svg, since we have it handled
-    fileLoaderRule.exclude = /\.svg$/i
-
-    // whyDidYouRender
-    if (options.dev && !options.isServer) {
-      const originalEntry = config.entry
-      config.entry = async () => {
-        const wdrPath = resolve(__dirname, './scripts/wdyr.js')
-        const entries = await originalEntry()
-        if (entries['main.js'] && !entries['main.js'].includes(wdrPath)) {
-          entries['main.js'].unshift(wdrPath)
+    },
+    i18n: {
+      locales: getSupportedLanguages(),
+      defaultLocale: DEFAULT_LOCALE,
+    },
+    webpack: (config, options) => {
+      const gitCommitSHAShort = process.env.RUN_GIT_COMMIT_SHA_SHORT ? execSync(process.env.RUN_GIT_COMMIT_SHA_SHORT) : ''
+      const gitCommitSHA = process.env.RUN_GIT_COMMIT_SHA ? execSync(process.env.RUN_GIT_COMMIT_SHA) : ''
+      const gitCommitRef = process.env.RUN_GIT_COMMIT_REF ? execSync(process.env.RUN_GIT_COMMIT_REF) : ''
+      const gitCommitTags = process.env.RUN_GIT_COMMIT_TAGS ? execSync(process.env.RUN_GIT_COMMIT_TAGS) : ''
+  
+      config.plugins.push(
+        new options.webpack.DefinePlugin({
+          'process.env.GIT_COMMIT_SHA_SHORT': JSON.stringify(
+            gitCommitSHAShort.toString()
+          ),
+          'process.env.GIT_COMMIT_SHA': JSON.stringify(gitCommitSHA.toString()),
+          'process.env.GIT_COMMIT_REF': JSON.stringify(gitCommitRef.toString()),
+          'process.env.GIT_COMMIT_TAGS': JSON.stringify(
+            gitCommitTags.toString()
+          ),
+          'process.env.DEFAULT_LOCALE': DEFAULT_LOCALE,
+          'process.env.LOCALES': JSON.stringify(getSupportedLanguages()),
+          'process.env.WDYR': JSON.stringify(process.env.WDYR),
+        })
+      )
+      
+      // SVG
+      // Grab the existing rule that handles SVG imports
+      const fileLoaderRule = config.module.rules.find((rule) =>
+        rule.test?.test?.('.svg'),
+      )
+  
+      config.module.rules.push(
+        // Convert all *.svg imports to React components
+        {
+          test: /\.svg$/i,
+          issuer: /\.[jt]sx?$/,
+          use: ['@svgr/webpack'],
+        },
+      )
+  
+      // Modify the file loader rule to ignore *.svg, since we have it handled
+      fileLoaderRule.exclude = /\.svg$/i
+  
+      // whyDidYouRender
+      if (options.dev && !options.isServer) {
+        const originalEntry = config.entry
+        config.entry = async () => {
+          const wdrPath = resolve(__dirname, './scripts/wdyr.js')
+          const entries = await originalEntry()
+          if (entries['main.js'] && !entries['main.js'].includes(wdrPath)) {
+            entries['main.js'].unshift(wdrPath)
+          }
+          return entries
         }
-        return entries
       }
-    }
-
-    if (!options.isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,  
-        fs: false,
+  
+      if (!options.isServer) {
+        config.resolve.fallback = {
+          ...config.resolve.fallback,  
+          fs: false,
+        }
       }
-    }
-
-    return config
+  
+      return config
+    },
+    productionBrowserSourceMaps: true,
   },
-  productionBrowserSourceMaps: true,
-  // disable the webpack plugins for client and server until the
-  // release mechanism is clear. At this point, there no need to
-  // upload sourcemaps for every single `next build` that happens
-  // e.g during E2E testing and vercel preview deployments.
-  sentry: {
-    disableClientWebpackPlugin: true,
-    disableServerWebpackPlugin: true
+  {
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options
+
+    // Suppresses source map uploading logs during build
+    silent: true,
+    org: "ooni",
+    project: "explorer",
+  },
+  {
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
+
+    // Transpiles SDK to be compatible with IE11 (increases bundle size)
+    transpileClientSDK: true,
+
+    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+    tunnelRoute: "/monitoring",
+
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+
+    // Enables automatic instrumentation of Vercel Cron Monitors.
+    // See the following for more information:
+    // https://docs.sentry.io/product/crons/
+    // https://vercel.com/docs/cron-jobs
+    automaticVercelMonitors: true,
   }
-}, SentryWebpackPluginOptions)
+);
