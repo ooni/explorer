da466cd305c69a097a3bb072fbb488c09f10148a
Maja <maja.komel@gmail.com>
2024-03-04 09:15:18 +0100

    Update Sentry config (#911)

--- pages/_error.js
+++ pages/_error.js
@@ -1,5 +1,5 @@
-import NextErrorComponent from 'next/error'
 import * as Sentry from '@sentry/nextjs'
+import NextErrorComponent from 'next/error'
 
 const ErrorPage = (props) => (
   <NextErrorComponent statusCode={props.statusCode} title={props.error} />
