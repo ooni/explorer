7e037be862c57d99e35fe6c4785245fc2df3cab6
Maja <maja.komel@gmail.com>
2024-09-06 09:56:07 +0200

    Update Sentry and ooni components (#958)

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -8,7 +8,7 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
 
 Sentry.init({
   dsn: SENTRY_DSN,
-  // Adjust this value in production, or use tracesSampler for greater control
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
