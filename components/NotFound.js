7016d34a66396ff22bc7d9985e7783064d8ad683
majakomel <maja.komel@gmail.com>
2024-10-25 11:15:24 +0200

    Fix navbar on measurement pages when measurement not found

--- components/NotFound.js
+++ components/NotFound.js
@@ -5,12 +5,14 @@ const NotFound = ({ title }) => {
   const { asPath } = useRouter()
 
   return (
-    <div className="container">
-      <div className="flex justify-around items-center my-16">
-        <OONI404 height="200px" />
-        <div className="w-1/2">
-          <h4>{title}</h4>
-          <div className="text-gray-800">{`${process.env.NEXT_PUBLIC_EXPLORER_URL}${asPath}`}</div>
+    <div className="shadow-[0_-100px_0] shadow-blue-500 mt-16">
+      <div className="container">
+        <div className="flex justify-around items-center py-20">
+          <OONI404 height="200px" />
+          <div className="w-1/2">
+            <h4>{title}</h4>
+            <div className="text-gray-800">{`${process.env.NEXT_PUBLIC_EXPLORER_URL}${asPath}`}</div>
+          </div>
         </div>
       </div>
     </div>
