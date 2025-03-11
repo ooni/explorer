9c72245ac242970e6d4cced5e806659e97d39a81
majakomel <maja.komel@gmail.com>
2025-03-11 11:37:06 +0100

    Improve language detection middleware with fallback language support

--- middleware.js
+++ middleware.js
@@ -7,9 +7,15 @@ export function middleware(request) {
     request.headers.get('accept-language')
   ) {
     const lang = request.headers.get('accept-language')
+    const fallbackLang = request.headers.get('accept-language').split('-')[0]
     if (process.env.LOCALES.includes(lang)) {
       return NextResponse.rewrite(
         new URL(`/${lang}${request.nextUrl.pathname}`, request.url),
+      )
+    }
+    if (process.env.LOCALES.includes(fallbackLang)) {
+      return NextResponse.rewrite(
+        new URL(`/${fallbackLang}${request.nextUrl.pathname}`, request.url),
       )
     }
   }
