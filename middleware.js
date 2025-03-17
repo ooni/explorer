a332f3083fb84676b64cbed4eb1046ddacb5cbad
majakomel <maja.komel@gmail.com>
2025-03-17 17:00:51 +0100

    Move middleware condition to config matcher

--- middleware.js
+++ middleware.js
@@ -1,22 +1,24 @@
 import { NextResponse } from 'next/server'
 
+export const config = {
+  matcher: ['/m/:path*', '/measurement/:path*'],
+  has: [
+    { type: 'header', key: 'enable-embedded-view' },
+    { type: 'header', key: 'accept-language' },
+  ],
+}
+
 export function middleware(request) {
-  if (
-    request.nextUrl.pathname.startsWith('/m/') &&
-    request.headers.get('enable-embedded-view') &&
-    request.headers.get('accept-language')
-  ) {
-    const lang = request.headers.get('accept-language')
-    const fallbackLang = request.headers.get('accept-language').split('-')[0]
-    if (process.env.LOCALES.includes(lang)) {
-      return NextResponse.rewrite(
-        new URL(`/${lang}${request.nextUrl.pathname}`, request.url),
-      )
-    }
-    if (process.env.LOCALES.includes(fallbackLang)) {
-      return NextResponse.rewrite(
-        new URL(`/${fallbackLang}${request.nextUrl.pathname}`, request.url),
-      )
-    }
+  const lang = request.headers.get('accept-language')
+  const fallbackLang = request.headers.get('accept-language').split('-')[0]
+  if (process.env.LOCALES.includes(lang)) {
+    return NextResponse.rewrite(
+      new URL(`/${lang}${request.nextUrl.pathname}`, request.url),
+    )
+  }
+  if (process.env.LOCALES.includes(fallbackLang)) {
+    return NextResponse.rewrite(
+      new URL(`/${fallbackLang}${request.nextUrl.pathname}`, request.url),
+    )
   }
 }
