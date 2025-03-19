0a9dee94b3c1ca256f3834ab56509d9221abaecf
majakomel <maja.komel@gmail.com>
2025-03-19 19:24:45 +0100

    Fix middleware

--- middleware.js
+++ middleware.js
@@ -10,12 +10,13 @@ export const config = {
 
 export function middleware(request) {
   const lang = request.headers.get('accept-language')
-  const fallbackLang = request.headers.get('accept-language').split('-')[0]
   if (process.env.LOCALES.includes(lang)) {
     return NextResponse.rewrite(
       new URL(`/${lang}${request.nextUrl.pathname}`, request.url),
     )
   }
+
+  const fallbackLang = request.headers.get('accept-language')?.split('-')[0]
   if (process.env.LOCALES.includes(fallbackLang)) {
     return NextResponse.rewrite(
       new URL(`/${fallbackLang}${request.nextUrl.pathname}`, request.url),
