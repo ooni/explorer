a73f6a10dd7a5fe463b44aa0dd239874b7f71174
Maja <maja.komel@gmail.com>
2025-03-19 19:40:34 +0100

    Add stricter condition to middleware, upgrade next (#1002)

--- middleware.js
+++ middleware.js
@@ -9,16 +9,22 @@ export const config = {
 }
 
 export function middleware(request) {
-  const lang = request.headers.get('accept-language')
-  const fallbackLang = request.headers.get('accept-language').split('-')[0]
-  if (process.env.LOCALES.includes(lang)) {
-    return NextResponse.rewrite(
-      new URL(`/${lang}${request.nextUrl.pathname}`, request.url),
-    )
-  }
-  if (process.env.LOCALES.includes(fallbackLang)) {
-    return NextResponse.rewrite(
-      new URL(`/${fallbackLang}${request.nextUrl.pathname}`, request.url),
-    )
+  if (
+    request.headers.get('accept-language') &&
+    request.headers.get('enable-embedded-view')
+  ) {
+    const lang = request.headers.get('accept-language')
+    if (process.env.LOCALES.includes(lang)) {
+      return NextResponse.rewrite(
+        new URL(`/${lang}${request.nextUrl.pathname}`, request.url),
+      )
+    }
+
+    const fallbackLang = request.headers.get('accept-language').split('-')[0]
+    if (process.env.LOCALES.includes(fallbackLang)) {
+      return NextResponse.rewrite(
+        new URL(`/${fallbackLang}${request.nextUrl.pathname}`, request.url),
+      )
+    }
   }
 }
