aa4d2ac19220abe9c76dc238d01a049486ca5e4f
majakomel <maja.komel@gmail.com>
2025-04-10 15:25:02 +0200

    Loading state for findings

--- components/FindingsSection.tsx
+++ components/FindingsSection.tsx
@@ -1,5 +1,6 @@
 import { FindingBoxSmall } from 'components/landing/HighlightBox'
 import Link from 'next/link'
+import { ChartSpinLoader } from './Chart'
 
 interface Finding {
   id: string
@@ -9,33 +10,41 @@ type FindingsSectionProps = {
   title: string
   theme: string
   findings: Finding[]
+  isLoading: boolean
 }
 
 const FindingsSection = ({
   title,
   theme,
   findings = [],
+  isLoading = false,
 }: FindingsSectionProps) => {
   return (
     <section className="mb-12">
       <h2>{title}</h2>
-      {findings.length ? (
-        <>
-          <div className="grid my-8 gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
-            {findings.map((finding) => (
-              <FindingBoxSmall key={finding.id} incident={finding} />
-            ))}
-            {theme && (
-              <div className="flex items-center justify-center px-24 text-center">
-                <Link href={`/findings?theme=${theme}`}>
-                  See all related censorship findings »
-                </Link>
-              </div>
-            )}
-          </div>
-        </>
+      {isLoading ? (
+        <ChartSpinLoader />
       ) : (
-        <div className="my-3">No findings available</div>
+        <>
+          {findings.length ? (
+            <>
+              <div className="grid my-8 gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+                {findings.map((finding) => (
+                  <FindingBoxSmall key={finding.id} incident={finding} />
+                ))}
+                {theme && (
+                  <div className="flex items-center justify-center px-24 text-center">
+                    <Link href={`/findings?theme=${theme}`}>
+                      See all related censorship findings »
+                    </Link>
+                  </div>
+                )}
+              </div>
+            </>
+          ) : (
+            <div className="my-3">No findings available</div>
+          )}
+        </>
       )}
     </section>
   )
