84a6821ef295749279a94749ff85b3bf569fae6c
majakomel <maja.komel@gmail.com>
2025-04-17 15:38:38 +0200

    Increase wait time before screenshot capture

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -49,7 +49,7 @@ export const config: CustomProjectConfig = {
   // OSS mode
   generateOnly: true,
   failOnDifference: true,
-  waitBeforeScreenshot: 3500,
+  waitBeforeScreenshot: 5000,
   lostPixelProjectId: 'cm9ldxgtv0s5s3wy0f1s8vo70',
   apiKey: process.env.LOST_PIXEL_API_KEY,
 
