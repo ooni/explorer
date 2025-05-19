569cf24bc55d7aa7a3885eb4c78db6a8568e0efc
Maja <maja.komel@gmail.com>
2025-05-19 11:02:33 +0200

    Add caret icon to language selector dropdown (#1025)

--- components/NavBar.js
+++ components/NavBar.js
@@ -9,22 +9,30 @@ import { getLocalisedLanguageName } from 'utils/i18nCountries'
 import { getDirection } from './withIntl'
 
 const LanguageSelect = (props) => (
-  <select
-    className="
-      appearance-none
-      bg-transparent
-      text-gray-50
-      opacity-60 
-      cursor-pointer 
-      capitalize 
-      outline-none
-      border-none
-      p-0
-      mb-1
-      hover:opacity-100
+  <div className="flex items-center mb-2 opacity-60 hover:opacity-100">
+    <select
+      className="
+        appearance-none
+        bg-transparent
+        text-gray-50
+        cursor-pointer
+        capitalize
+        outline-none
+        border-none
+        p-0
+      "
+      {...props}
+    />
+    <span
+      className="
+      w-2
+      h-2
+      bg-[url('data:image/svg+xml;charset=UTF-8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2212%22%20height%3D%2212%22%20viewBox%3D%220%200%2012%2012%22%3E%3Ctitle%3Edown-arrow%3C%2Ftitle%3E%3Cg%20fill%3D%22%23FFFFFF%22%3E%3Cpath%20d%3D%22M10.293%2C3.293%2C6%2C7.586%2C1.707%2C3.293A1%2C1%2C0%2C0%2C0%2C.293%2C4.707l5%2C5a1%2C1%2C0%2C0%2C0%2C1.414%2C0l5-5a1%2C1%2C0%2C1%2C0-1.414-1.414Z%22%20fill%3D%22%23FFFFFF%22%3E%3C%2Fpath%3E%3C%2Fg%3E%3C%2Fsvg%3E')]
+      bg-no-repeat
+      bg-contain
     "
-    {...props}
-  />
+    />
+  </div>
 )
 
 const StyledNavItem = ({ isActive, ...props }) => (
