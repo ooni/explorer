9fc0ae40601e66200676edfacf7c6dab190d0f97
majakomel <maja.komel@gmail.com>
2025-11-21 09:30:39 +0100

    Update donation banner to mirror design from ooni.org

--- components/DonationBanner.js
+++ components/DonationBanner.js
@@ -19,55 +19,59 @@ export const DonationBanner = () => {
           }}
         >
           <button
-            class="absolute top-4 right-6 text-[#C3FAE8] hover:text-white transition-colors cursor-pointer bg-transparent border-none text-4xl"
+            className="absolute top-4 right-6 text-[#C3FAE8] hover:text-white transition-colors cursor-pointer bg-transparent border-none text-4xl"
             aria-label="Close donation banner"
             type="button"
             onClick={() => setIsOpen(false)}
           >
             ×
           </button>
-          <div class="max-w-[990px] px-8 mx-auto">
-            <div class="flex md:flex-row flex-col-reverse">
-              <div class="md:w-[52%] w-full flex items-end">
-                <img
-                  src="/static/images/donate_banner/tentacle.png"
-                  alt="Tentacle"
-                />
-              </div>
-              <div class="md:w-[48%] md:ml-[-20px] w-full py-8 flex flex-col">
+          <div className="max-w-[990px] px-8 mx-auto">
+            <div className="flex md:flex-row flex-col-reverse">
+              <div className="md:w-[50%] md:ml-[-20px] w-full py-8 flex flex-col">
                 <div>
                   <img
                     src="/static/images/donate_banner/slogan.png"
                     alt="Protect Our Internet; Document Censorship"
-                    class="md:ml-[-40px] md:w-[90%] mb-8 md:mb-4"
+                    className="md:w-[90%] mb-8 md:mb-4"
                   />
                 </div>
-                <div class="md:w-10/12">
-                  <p class="mb-2">
+                <div className="md:w-10/12">
+                  <p className="mb-2">
                     Around the world, access to information is increasingly
                     blocked and manipulated.
                   </p>
-                  <p class="mb-2">
+                  <p className="mb-2">
                     Transparency of Network Interference is more important than
                     ever.
                   </p>
-                  <p class="mb-2">
+                  <p className="mb-2">
                     Support the world's largest open dataset on Internet
                     censorship. Donate to OONI and help keep the Internet open
                     and transparent.
                   </p>
                   <a
                     href="https://ooni.org/donate/"
-                    class="block mx-auto mt-8 w-fit"
+                    className="block mt-8 w-fit"
                   >
                     <button
-                      class="bg-[#15FFAB] text-black rounded-sm px-4 py-2 hover:cursor-pointer"
+                      className="bg-[#FFC400] text-black rounded-sm px-4 py-2 hover:cursor-pointer
+                     transition-all duration-200 ease-out
+                     hover:scale-110 hover:-translate-y-1
+                     hover:shadow-[0_0_20px_5px_rgba(255,196,0,0.7)]"
                       type="button"
                     >
                       Donate now
                     </button>
                   </a>
                 </div>
+              </div>
+              <div className="md:w-[50%] md:ml-[-100px] w-full flex items-end">
+                <img
+                  src="/static/images/donate_banner/tentacle.png"
+                  className="scale-x-[-1]"
+                  alt="Tentacle"
+                />
               </div>
             </div>
           </div>
