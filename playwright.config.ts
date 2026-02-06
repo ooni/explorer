35da5bad6ad49f95f5cb98b9c8957f4dd8d48288
majakomel <maja.komel@gmail.com>
2026-02-06 11:24:47 +0100

    Increase timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ import { defineConfig, devices } from '@playwright/test'
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  timeout: 60000,
+  timeout: 90000,
 
   testDir: './tests',
   /* Run tests in files in parallel */
