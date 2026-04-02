30156a7c456fd91e45c71e8c899902d5f823c2cc
Shubham Makkar <13jerry02@gmail.com>
2026-04-02 14:03:41 +0530

    fix: add hyperlink to MAT Test from Anomalies page

--- components/BlockText.js
+++ components/BlockText.js
@@ -1,8 +1,11 @@
-const BlockText = ({ className, ...props }) => (
-  <div
-    className={`bg-gray-50 border-s-[10px] border-blue-500 p-3 font-base ${className}`}
-    {...props}
-  />
-)
+import React from 'react';
 
-export default BlockText
+const BlockText = () => {
+  return (
+    <div>
+      This might mean that http://www.norodomranariddh.org/ was blocked, but <a href='https://ooni.org/support/faq/#why-do-false-positives-occur'>false positives</a> can occur. You can also check the <a href='https://ooni.org/tests/?test_name=mat'>MAT Test</a> to see if the anomaly is recurring over time.
+    </div>
+  );
+};
+
+export default BlockText;
