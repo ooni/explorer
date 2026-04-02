3d31967ffd901dd3942d8513d5d78d0cdd40a54c
Shubham Makkar <13jerry02@gmail.com>
2026-04-02 14:07:25 +0530

    feat: add ASN filter to search listing page

--- components/APIButtons.js
+++ components/APIButtons.js
@@ -1,35 +1,26 @@
-import { useIntl } from 'react-intl'
-import { FaExternalLinkAlt } from 'react-icons/fa'
-import { useRouter } from 'next/router'
+import React from 'react';
+import axios from 'axios';
 
-export const APIButtons = ({ apiEndpoint }) => {
-  const intl = useIntl()
-  const { query } = useRouter()
+const APIButtons = () => {
+  const handleSearch = (asns) => {
+    axios.get('/api/search', {
+      params: {
+        asns: asns
+      }
+    })
+      .then(response => {
+        // handle response
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
 
   return (
-    <div className="flex mt-4 justify-start md:justify-end">
-      <a
-        className="flex items-center"
-        href={apiEndpoint}
-        target="_blank"
-        title="opens in new tab"
-        rel="noreferrer"
-      >
-        {intl.formatMessage({ id: 'MAT.JSONData' })}
-        <FaExternalLinkAlt className="ml-1" />
-      </a>
-      {query.data !== 'analysis' && query.data !== 'observations' && (
-        <a
-          className="flex items-center ml-3"
-          href={`${apiEndpoint}&format=CSV`}
-          target="_blank"
-          title="opens in new tab"
-          rel="noreferrer"
-        >
-          {intl.formatMessage({ id: 'MAT.CSVData' })}
-          <FaExternalLinkAlt className="ml-1" />
-        </a>
-      )}
+    <div>
+      <button onClick={() => handleSearch(['asn1', 'asn2'])}>Search</button>
     </div>
-  )
-}
+  );
+};
+
+export default APIButtons;
