11d8def10e9a4f020e6a64d3c21e542ba04db069
majakomel <maja.komel@gmail.com>
2026-05-29 13:03:53 +0200

    Temporarily hide mat chart error details

--- components/aggregation/mat/NoCharts.js
+++ components/aggregation/mat/NoCharts.js
@@ -2,7 +2,7 @@ import { FormattedMessage } from 'react-intl'
 
 export const NoCharts = ({ message }) => {
   return (
-    <div className="flex flex-col justify-center h-[100%]">
+    <div className="flex flex-col justify-center h-full">
       <div className="text-xl font-bold mb-2">
         <FormattedMessage id="MAT.Charts.NoData.Title" />
       </div>
@@ -10,12 +10,12 @@ export const NoCharts = ({ message }) => {
         <div className="mb-2">
           <FormattedMessage id="MAT.Charts.NoData.Description" />
         </div>
-        {message && (
+        {/* {message && (
           <div className="p-2 m-2 text-sm">
             <FormattedMessage id="MAT.Charts.NoData.Details" />
             <pre className="my-2">{message}</pre>
           </div>
-        )}
+        )} */}
       </div>
     </div>
   )
